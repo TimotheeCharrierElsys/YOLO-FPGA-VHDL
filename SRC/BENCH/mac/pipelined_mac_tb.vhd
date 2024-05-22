097552bf6348510d2580df89519a48d0fae2768b
TimotheeCharrierElsys <timothee.charrier@elsys-design.com>
2024-05-22 15:54:04 +0200

    feat: add fully pipelined MAC entity

---
+++ SRC/BENCH/mac/pipelined_mac_tb.vhd
@@ -0,0 +1,114 @@
+-----------------------------------------------------------------------------------
+--!     @Testbench    mac_tb
+--!     @brief        This testbench verifies the functionality of the pipelined_mac
+--!     @details      It initializes the inputs, applies test vectors, and checks the outputs.
+--!     @auth         Timothée Charrier
+-----------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library LIB_RTL;
+use LIB_RTL.binary_adder_tree_pkg.all;
+
+entity pipelined_mac_tb is
+end entity;
+
+architecture pipelined_mac_tb_arch of pipelined_mac_tb is
+    -------------------------------------------------------------------------------------
+    -- CONSTANTS
+    -------------------------------------------------------------------------------------
+    constant i_clk_period : time    := 10 ns; --! Clock period
+    constant WAIT_COUNT   : integer := 8;     --! Number clock tics to wait
+    constant BITWIDTH     : integer := 8;     --! Bit BITWIDTH of each operand
+
+    -------------------------------------------------------------------------------------
+    -- SIGNALS
+    -------------------------------------------------------------------------------------
+    signal i_clk    : std_logic := '0';                            --! Clock signal
+    signal i_rst    : std_logic := '1';                            --! Reset signal, active at high state
+    signal i_enable : std_logic := '0';                            --! Enable signal, active at high state
+    signal i_A      : std_logic_vector(BITWIDTH - 1 downto 0);     --! First mult operand
+    signal i_B      : std_logic_vector(BITWIDTH - 1 downto 0);     --! Second mult operand
+    signal i_C      : std_logic_vector(BITWIDTH - 1 downto 0);     --! Third operand
+    signal o_P      : std_logic_vector(2 * BITWIDTH - 1 downto 0); --! Output data
+
+    -------------------------------------------------------------------------------------
+    -- COMPONENTS
+    -------------------------------------------------------------------------------------
+    component pipelined_mac
+        generic (
+            BITWIDTH : integer
+        );
+        port (
+            i_clk    : in std_logic;
+            i_rst    : in std_logic;
+            i_enable : in std_logic;
+            i_A      : in std_logic_vector(BITWIDTH - 1 downto 0);
+            i_B      : in std_logic_vector(BITWIDTH - 1 downto 0);
+            i_C      : in std_logic_vector(BITWIDTH - 1 downto 0);
+            o_P      : out std_logic_vector(2 * BITWIDTH - 1 downto 0)
+        );
+    end component;
+
+begin
+    -------------------------------------------------------------------------------------
+    -- UNIT UNDER TEST (UUT)
+    -------------------------------------------------------------------------------------
+    UUT : pipelined_mac
+    generic map(
+        BITWIDTH => BITWIDTH
+    )
+    port map(
+        i_clk    => i_clk,
+        i_rst    => i_rst,
+        i_enable => i_enable,
+        i_A      => i_A,
+        i_B      => i_B,
+        i_C      => i_C,
+        o_P      => o_P
+    );
+
+    -- Clock generation
+    i_clk <= not i_clk after i_clk_period / 2;
+
+    -------------------------------------------------------------------------------------
+    -- TEST PROCESS
+    -------------------------------------------------------------------------------------
+    stimulus : process
+    begin
+        -- Reset the system
+        i_rst <= '1';
+        wait for 2 * i_clk_period;
+        i_rst <= '0';
+
+        -- Enable the pipelined_mac unit
+        i_enable <= '1';
+
+        -- Apply input vectors
+        i_A <= std_logic_vector(to_signed(5, BITWIDTH));
+        i_B <= std_logic_vector(to_signed(7, BITWIDTH));
+        i_C <= std_logic_vector(to_signed(10, BITWIDTH));
+
+        -- Wait for enough time to allow the pipeline to process the inputs
+        wait for (WAIT_COUNT * i_clk_period);
+
+        -- Check the output
+        assert o_P = std_logic_vector(to_signed(45, 2 * BITWIDTH))
+        report "Test failed: output does not match expected output"
+            severity error;
+
+        -- Finish the simulation
+        wait;
+    end process stimulus;
+
+end architecture;
+
+configuration pipelined_mac_tb_conf of pipelined_mac_tb is
+    for pipelined_mac_tb_arch
+        for UUT : pipelined_mac
+            use entity LIB_RTL.pipelined_mac(mac_arch);
+        end for;
+    end for;
+end configuration pipelined_mac_tb_conf;
---
+++ SRC/RTL/mac/pipelined_mac.vhd
@@ -0,0 +1,81 @@
+-----------------------------------------------------------------------------------
+--!     @Entity     pipelined_mac
+--!     @brief      This entity implements a pipelined Multiply-Accumulate (pipelined_mac) unit.
+--!                 It performs multiplication of two operands followed by an addition
+--!                 with a third operand. The design is pipelined to improve performance.
+-----------------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
+
+--! @Entity pipelined_mac
+--! @brief This entity implements a pipelined Multiply-Accumulate (pipelined_mac) unit.
+--!        It multiplies two operands and then adds a third operand.
+entity pipelined_mac is
+    generic (
+        BITWIDTH : integer := 8 --! Bit width of each operand
+    );
+    port (
+        i_clk    : in std_logic;                                   --! Clock signal
+        i_rst    : in std_logic;                                   --! Reset signal, active at high state
+        i_enable : in std_logic;                                   --! Enable signal, active at high state
+        i_A      : in std_logic_vector(BITWIDTH - 1 downto 0);     --! First multiplication operand
+        i_B      : in std_logic_vector(BITWIDTH - 1 downto 0);     --! Second multiplication operand
+        i_C      : in std_logic_vector(BITWIDTH - 1 downto 0);     --! Accumulation operand
+        o_P      : out std_logic_vector(2 * BITWIDTH - 1 downto 0) --! Output result
+    );
+end pipelined_mac;
+
+architecture pipelined_mac_arch of pipelined_mac is
+
+    -------------------------------------------------------------------------------------
+    -- SIGNALS
+    -------------------------------------------------------------------------------------
+
+    signal r_A            : std_logic_vector(BITWIDTH - 1 downto 0);     --! Registered version of input operand A.
+    signal r_B            : std_logic_vector(BITWIDTH - 1 downto 0);     --! Registered version of input operand B.
+    signal r_C            : std_logic_vector(BITWIDTH - 1 downto 0);     --! Registered version of input operand C.
+    signal r_mult         : std_logic_vector(2 * BITWIDTH - 1 downto 0); --! Result of the multiplication of r_A and r_B.
+    signal mult_stage_reg : std_logic_vector(2 * BITWIDTH - 1 downto 0); --! Registered output of the multiplication stage.
+    signal add_stage_reg  : std_logic_vector(2 * BITWIDTH - 1 downto 0); --! Registered output of the addition stage.
+
+begin
+
+    -------------------------------------------------------------------------------------
+    -- PROCESS ASYNC (reset high)
+    -------------------------------------------------------------------------------------
+    --! @process
+    --! @brief Handles the synchronous and asynchronous operations of the pipelined pipelined_mac unit.
+    process (i_clk, i_rst)
+    begin
+        if i_rst = '1' then
+            -- Reset all registers to zero
+            r_A            <= (others => '0');
+            r_B            <= (others => '0');
+            r_C            <= (others => '0');
+            r_mult         <= (others => '0');
+            mult_stage_reg <= (others => '0');
+            add_stage_reg  <= (others => '0');
+        elsif rising_edge(i_clk) then
+            if (i_enable = '1') then
+                -- Stage 1: Register inputs
+                r_A <= i_A;
+                r_B <= i_B;
+                r_C <= i_C;
+
+                -- Stage 2: Perform multiplication and register the result
+                mult_stage_reg <= std_logic_vector(signed(r_A) * signed(r_B));
+
+                -- Stage 3: Perform addition with registered multiplier output and register the result
+                add_stage_reg <= std_logic_vector(signed(mult_stage_reg) + signed(r_C));
+            end if;
+        end if;
+    end process;
+
+    -------------------------------------------------------------------------------------
+    -- OUTPUT ASSIGNMENT
+    -------------------------------------------------------------------------------------
+    o_P <= add_stage_reg;
+
+end pipelined_mac_arch;
