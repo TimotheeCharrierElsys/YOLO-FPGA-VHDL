d08495ebc17432722970497e5e712ac2054cfdc2
TimotheeCharrierElsys <timothee.charrier@elsys-design.com>
2024-05-22 15:54:38 +0200

    feat: add N*N convolution using Pipelined MAC units

---
+++ SRC/BENCH/mac/pipelined_mac_33_tb.vhd
@@ -0,0 +1,121 @@
+-----------------------------------------------------------------------------------
+--!     @Testbench    pipelined_mac33_tb
+--!     @brief        This testbench verifies the functionality of the pipelined mac 3*3
+--!     @details      It initializes the inputs, applies test vectors, and checks the outputs.
+--!     @auth         Timothée Charrier
+-----------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library LIB_RTL;
+use LIB_RTL.binary_adder_tree_pkg.all;
+
+entity pipelined_mac33_tb is
+end entity;
+
+architecture pipelined_mac33_tb_arch of pipelined_mac33_tb is
+    -------------------------------------------------------------------------------------
+    -- CONSTANTS
+    -------------------------------------------------------------------------------------
+    constant i_clk_period : time    := 10 ns; --! Clock period
+    constant WAIT_COUNT   : integer := 8;     --! Number clock tics to wait
+    constant BITWIDTH     : integer := 8;     --! Bit BITWIDTH of each operand
+    constant KERNEL_SIZE  : integer := 3;     --! Kernel Size
+
+    -------------------------------------------------------------------------------------
+    -- SIGNALS
+    -------------------------------------------------------------------------------------
+    signal i_clk    : std_logic := '0'; --! Clock signal
+    signal i_rst    : std_logic := '1'; --! Reset signal, active at high state
+    signal i_enable : std_logic := '0'; --! Enable signal, active at high state
+    signal i_X      : t_vec (0 to KERNEL_SIZE * KERNEL_SIZE - 1)(BITWIDTH - 1 downto 0);
+    signal i_theta  : t_vec (0 to KERNEL_SIZE * KERNEL_SIZE - 1)(BITWIDTH - 1 downto 0);
+    signal o_Y      : std_logic_vector (2 * BITWIDTH - 1 downto 0);
+
+    -------------------------------------------------------------------------------------
+    -- COMPONENTS
+    -------------------------------------------------------------------------------------
+    component pipelined_mac33
+        generic (
+            BITWIDTH    : integer;
+            KERNEL_SIZE : integer
+        );
+        port (
+            i_clk    : in std_logic;
+            i_rst    : in std_logic;
+            i_enable : in std_logic;
+            i_X      : in t_vec (0 to KERNEL_SIZE * KERNEL_SIZE - 1)(BITWIDTH - 1 downto 0);
+            i_theta  : in t_vec (0 to KERNEL_SIZE * KERNEL_SIZE - 1)(BITWIDTH - 1 downto 0);
+            o_Y      : out std_logic_vector (2 * BITWIDTH - 1 downto 0)
+        );
+    end component;
+
+begin
+    -------------------------------------------------------------------------------------
+    -- UNIT UNDER TEST (UUT)
+    -------------------------------------------------------------------------------------
+    UUT : pipelined_mac33
+    generic map(
+        BITWIDTH    => BITWIDTH,
+        KERNEL_SIZE => KERNEL_SIZE
+    )
+    port map(
+        i_clk    => i_clk,
+        i_rst    => i_rst,
+        i_enable => i_enable,
+        i_X      => i_X,
+        i_theta  => i_theta,
+        o_Y      => o_Y
+    );
+
+    -- Clock generation
+    i_clk <= not i_clk after i_clk_period / 2;
+
+    -------------------------------------------------------------------------------------
+    -- TEST PROCESS
+    -------------------------------------------------------------------------------------
+    stimulus : process
+    begin
+        -- Reset the system
+        i_rst <= '1';
+        wait for 2 * i_clk_period;
+        i_rst <= '0';
+
+        -- Enable the mac unit
+        i_enable <= '1';
+
+        -- Apply input vectors
+        i_X        <= (others => std_logic_vector(to_unsigned(1, BITWIDTH)));
+        i_theta(0) <= std_logic_vector(to_unsigned(1, BITWIDTH));
+        i_theta(1) <= std_logic_vector(to_unsigned(0, BITWIDTH));
+        i_theta(2) <= std_logic_vector(to_unsigned(0, BITWIDTH));
+        i_theta(3) <= std_logic_vector(to_unsigned(0, BITWIDTH));
+        i_theta(4) <= std_logic_vector(to_unsigned(1, BITWIDTH));
+        i_theta(5) <= std_logic_vector(to_unsigned(0, BITWIDTH));
+        i_theta(6) <= std_logic_vector(to_unsigned(0, BITWIDTH));
+        i_theta(7) <= std_logic_vector(to_unsigned(0, BITWIDTH));
+        i_theta(8) <= std_logic_vector(to_unsigned(1, BITWIDTH));
+
+        -- Wait for enough time to allow the pipeline to process the inputs
+        wait for (WAIT_COUNT * i_clk_period);
+
+        -- Check the output
+        assert o_Y = std_logic_vector(to_signed(18, 2 * BITWIDTH))
+        report "Test failed: output does not match expected output"
+            severity error;
+
+        -- Finish the simulation
+        wait;
+    end process stimulus;
+
+end architecture;
+
+configuration pipelined_mac33_tb_conf of pipelined_mac33_tb is
+    for pipelined_mac33_tb_arch
+        for UUT : pipelined_mac33
+            use configuration LIB_RTL.pipelined_mac33_conf;
+        end for;
+    end for;
+end configuration pipelined_mac33_tb_conf;
---
+++ SRC/RTL/mac/pipelined_mac33.vhd
@@ -0,0 +1,137 @@
+-----------------------------------------------------------------------------------
+--!     @Package    pipelined_mac33
+--!     @brief      This file provides the conv adder tree entity and architecture
+--!     @author     Timothée Charrier
+-----------------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
+use ieee.math_real.all;
+
+library LIB_RTL;
+use LIB_RTL.binary_adder_tree_pkg.all;
+
+--! @Entity pipelined_mac33
+--! @brief This entity implements a pipelined Multiply-Accumulate (MAC) unit with a 3x3 kernel.
+--!        It performs convolution operations using a 3x3 kernel over the input data.
+entity pipelined_mac33 is
+    generic (
+        BITWIDTH    : integer := 8; --! Bit width of each operand
+        KERNEL_SIZE : integer := 3  --! Size of the kernel (ex: 3 for a 3x3 kernel)
+    );
+    port (
+        i_clk    : in std_logic;                                                         --! Clock signal
+        i_rst    : in std_logic;                                                         --! Reset signal, active at high state
+        i_enable : in std_logic;                                                         --! Enable signal, active at high state
+        i_X      : in t_vec (0 to KERNEL_SIZE * KERNEL_SIZE - 1)(BITWIDTH - 1 downto 0); --! Input data (3x3xBITWIDTH bits)
+        i_theta  : in t_vec (0 to KERNEL_SIZE * KERNEL_SIZE - 1)(BITWIDTH - 1 downto 0); --! Kernel data (3x3xBITWIDTH bits)
+        o_Y      : out std_logic_vector (2 * BITWIDTH - 1 downto 0)                      --! Output result
+    );
+end pipelined_mac33;
+
+architecture pipelined_mac33_arch of pipelined_mac33 is
+
+    -------------------------------------------------------------------------------------
+    -- SIGNALS
+    -------------------------------------------------------------------------------------
+    signal mac_out : t_vec(0 to KERNEL_SIZE * KERNEL_SIZE - 1)(2 * BITWIDTH - 1 downto 0); --! Intermediate signal to hold the output of each MAC unit in the pipeline.
+
+    -------------------------------------------------------------------------------------
+    -- COMPONENTS
+    -------------------------------------------------------------------------------------
+
+    component mac
+        generic (
+            BITWIDTH : integer --! Bit width of each operand
+        );
+        port (
+            i_clk    : in std_logic;                                   --! Clock signal
+            i_rst    : in std_logic;                                   --! Reset signal, active at high state
+            i_enable : in std_logic;                                   --! Enable signal, active at high state
+            i_A      : in std_logic_vector(BITWIDTH - 1 downto 0);     --! First multiplication operand
+            i_B      : in std_logic_vector(BITWIDTH - 1 downto 0);     --! Second multiplication operand
+            i_C      : in std_logic_vector(BITWIDTH - 1 downto 0);     --! Accumulation operand
+            o_P      : out std_logic_vector(2 * BITWIDTH - 1 downto 0) --! Output result
+        );
+    end component;
+
+begin
+
+    -------------------------------------------------------------------------------------
+    -- GENERATE BLOCK
+    -------------------------------------------------------------------------------------
+    pipelined_mac : for i in 0 to KERNEL_SIZE * KERNEL_SIZE - 1 generate
+
+        --! Instantiate the first MAC unit without an accumulation operand.
+        first_mac : if i = 0 generate
+            first_custom_mac_inst : mac --! Multiply without additions
+            generic map(BITWIDTH => BITWIDTH)
+            port map(
+                i_clk    => i_clk,
+                i_rst    => i_rst,
+                i_enable => i_enable,
+                i_A      => i_X(i),
+                i_B      => i_theta(i),
+                i_C => (others => '0'),
+                o_P      => mac_out(i)
+            );
+        end generate first_mac;
+
+        --! Instantiate the intermediate MAC units with accumulation of previous results.
+        gen_mac : if i > 0 and i < KERNEL_SIZE * KERNEL_SIZE - 1 generate
+            gen_mac_inst : mac --! Multiply then add the previous result
+            generic map(BITWIDTH => BITWIDTH)
+            port map(
+                i_clk    => i_clk,
+                i_rst    => i_rst,
+                i_enable => i_enable,
+                i_A      => i_X(i),
+                i_B      => i_theta(i),
+                i_C      => mac_out(i - 1)(BITWIDTH - 1 downto 0),
+                o_P      => mac_out(i)
+            );
+        end generate gen_mac;
+
+        --! Instantiate the last MAC unit and output the final result.
+        last_mac : if i = KERNEL_SIZE * KERNEL_SIZE - 1 generate
+            last_mac_inst : mac --! Convolution output is the result of the last MAC unit
+            generic map(BITWIDTH => BITWIDTH)
+            port map(
+                i_clk    => i_clk,
+                i_rst    => i_rst,
+                i_enable => i_enable,
+                i_A      => i_X(i),
+                i_B      => i_theta(i),
+                i_C      => mac_out(i - 1)(BITWIDTH - 1 downto 0),
+                o_P      => o_Y
+            );
+        end generate last_mac;
+    end generate pipelined_mac;
+
+end pipelined_mac33_arch;
+
+configuration pipelined_mac33_conf of pipelined_mac33 is
+    for pipelined_mac33_arch
+        for pipelined_mac
+
+            for first_mac
+                for all : mac
+                    use entity LIB_RTL.mac(mac_arch);
+                end for;
+            end for;
+
+            for gen_mac
+                for all : mac
+                    use entity LIB_RTL.mac(mac_arch);
+                end for;
+            end for;
+
+            for last_mac
+                for all : mac
+                    use entity LIB_RTL.mac(mac_arch);
+                end for;
+            end for;
+        end for;
+    end for;
+end configuration pipelined_mac33_conf;
