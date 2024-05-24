f6d0df5c80861f0ddfaa463fa0edba7365aac925
TimotheeCharrierElsys <timothee.charrier@elsys-design.com>
2024-05-24 14:34:40 +0200

    feat: add fully connected layer VHDL files

---
+++ SRC/BENCH/mac/fc_layer_tb.vhd
@@ -0,0 +1,120 @@
+-----------------------------------------------------------------------------------
+--!     @file    fc_layer_tb
+--!     @brief        This testbench verifies the functionality of the pipelined mac 3*3
+--!     @details      It initializes the inputs, applies test vectors, and checks the outputs.
+--!     @author       Timothée Charrier
+-----------------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
+
+library LIB_RTL;
+use LIB_RTL.types_pkg.all;
+
+entity fc_layer_tb is
+end entity;
+
+architecture fc_layer_tb_arch of fc_layer_tb is
+    -------------------------------------------------------------------------------------
+    -- CONSTANTS
+    -------------------------------------------------------------------------------------
+    constant i_clk_period : time    := 10 ns; --! Clock period
+    constant WAIT_COUNT   : integer := 8;     --! Number clock tics to wait
+    constant BITWIDTH     : integer := 8;     --! Bit BITWIDTH of each operand
+    constant VECTOR_SIZE  : integer := 8;     --! Kernel Size
+
+    -------------------------------------------------------------------------------------
+    -- SIGNALS
+    -------------------------------------------------------------------------------------
+    signal i_clk    : std_logic := '0'; --! Clock signal
+    signal i_rst    : std_logic := '1'; --! Reset signal, active at high state
+    signal i_enable : std_logic := '0'; --! Enable signal, active at high state
+    signal i_data   : t_vec (VECTOR_SIZE - 1 downto 0)(BITWIDTH - 1 downto 0);
+    signal i_weight : t_vec (VECTOR_SIZE - 1 downto 0)(BITWIDTH - 1 downto 0);
+    signal o_sum    : std_logic_vector (2 * BITWIDTH - 1 downto 0);
+
+    -------------------------------------------------------------------------------------
+    -- COMPONENTS
+    -------------------------------------------------------------------------------------
+    component fc_layer
+        generic (
+            BITWIDTH    : integer;
+            VECTOR_SIZE : integer
+        );
+        port (
+            i_clk    : in std_logic;
+            i_rst    : in std_logic;
+            i_enable : in std_logic;
+            i_data   : in t_vec(VECTOR_SIZE - 1 downto 0)(BITWIDTH - 1 downto 0);
+            i_weight : in t_vec(VECTOR_SIZE - 1 downto 0)(BITWIDTH - 1 downto 0);
+            o_sum    : out std_logic_vector(2 * BITWIDTH - 1 downto 0)
+        );
+    end component;
+
+begin
+    -------------------------------------------------------------------------------------
+    -- UNIT UNDER TEST (UUT)
+    -------------------------------------------------------------------------------------
+    UUT : fc_layer
+    generic map(
+        BITWIDTH    => BITWIDTH,
+        VECTOR_SIZE => VECTOR_SIZE
+    )
+    port map(
+        i_clk    => i_clk,
+        i_rst    => i_rst,
+        i_enable => i_enable,
+        i_data   => i_data,
+        i_weight => i_weight,
+        o_sum    => o_sum
+    );
+
+    -- Clock generation
+    i_clk <= not i_clk after i_clk_period / 2;
+
+    -------------------------------------------------------------------------------------
+    -- TEST PROCESS
+    -------------------------------------------------------------------------------------
+    stimulus : process
+    begin
+        -- Reset the system
+        i_rst <= '1';
+        wait for 2 * i_clk_period;
+        i_rst <= '0';
+
+        -- Enable the mac unit
+        i_enable <= '1';
+
+        -- Apply input vectors
+        i_data      <= (others => std_logic_vector(to_unsigned(1, BITWIDTH)));
+        i_weight(0) <= std_logic_vector(to_unsigned(1, BITWIDTH));
+        i_weight(1) <= std_logic_vector(to_unsigned(2, BITWIDTH));
+        i_weight(2) <= std_logic_vector(to_unsigned(3, BITWIDTH));
+        i_weight(3) <= std_logic_vector(to_unsigned(4, BITWIDTH));
+        i_weight(4) <= std_logic_vector(to_unsigned(5, BITWIDTH));
+        i_weight(5) <= std_logic_vector(to_unsigned(6, BITWIDTH));
+        i_weight(6) <= std_logic_vector(to_unsigned(7, BITWIDTH));
+        i_weight(7) <= std_logic_vector(to_unsigned(8, BITWIDTH));
+
+        -- Wait for enough time to allow the pipeline to process the inputs
+        wait for (WAIT_COUNT * i_clk_period);
+
+        -- Check the output
+        assert o_sum = std_logic_vector(to_signed(36, 2 * BITWIDTH))
+        report "Test failed: output does not match expected output"
+            severity error;
+
+        -- Finish the simulation
+        wait;
+    end process stimulus;
+
+end architecture;
+
+configuration fc_layer_tb_conf of fc_layer_tb is
+    for fc_layer_tb_arch
+        for UUT : fc_layer
+            use configuration LIB_RTL.fc_layer_conf;
+        end for;
+    end for;
+end configuration fc_layer_tb_conf;
---
+++ SRC/RTL/mac/fc_layer.vhd
@@ -0,0 +1,96 @@
+-----------------------------------------------------------------------------------
+--!     @file       fc_layer
+--!     @brief      This entity implements a pipelined fully connected layer.
+--!                 It performs multiplication and then additions
+--!     @author     Timothée Charrier
+-----------------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
+
+library LIB_RTL;
+use LIB_RTL.types_pkg.all;
+
+--! Entity fc_layer
+--! This entity implements a full connected layer layer using an adder tree.
+entity fc_layer is
+    generic (
+        BITWIDTH    : integer := 8; --! Bit width of each operand
+        VECTOR_SIZE : integer := 8  --! Input Vector Size
+    );
+    port (
+        i_clk    : in std_logic;                                              --! Clock signal
+        i_rst    : in std_logic;                                              --! Reset signal, active at high state
+        i_enable : in std_logic;                                              --! Enable signal, active at high state
+        i_data   : in t_vec(VECTOR_SIZE - 1 downto 0)(BITWIDTH - 1 downto 0); --! Input data
+        i_weight : in t_vec(VECTOR_SIZE - 1 downto 0)(BITWIDTH - 1 downto 0); --! Input weights
+        o_sum    : out std_logic_vector(2 * BITWIDTH - 1 downto 0)            --! Output value
+    );
+end fc_layer;
+
+architecture fc_layer_arch of fc_layer is
+
+    -------------------------------------------------------------------------------------
+    -- SIGNALS
+    -------------------------------------------------------------------------------------
+    signal r_mult_to_add : t_vec(VECTOR_SIZE - 1 downto 0)(2 * BITWIDTH - 1 downto 0); --! Signal between the multiplications and the additions
+    signal r_sum         : std_logic_vector(2 * BITWIDTH - 1 downto 0);                --! Output signal register
+
+    -------------------------------------------------------------------------------------
+    -- COMPONENTS
+    -------------------------------------------------------------------------------------
+    component adder_tree
+        generic (
+            N_OPD    : integer;
+            BITWIDTH : integer
+        );
+        port (
+            i_clk  : in std_logic;                                    --! Clock signal
+            i_rst  : in std_logic;                                    --! Reset signal, active at high state
+            i_data : in t_vec(0 to N_OPD - 1)(BITWIDTH - 1 downto 0); --! Input data vector
+            o_data : out std_logic_vector(BITWIDTH - 1 downto 0)      --! Output data
+        );
+    end component;
+
+begin
+
+    -------------------------------------------------------------------------------------
+    -- MULTIPLICATION GENERATION
+    -------------------------------------------------------------------------------------
+    -- Multiply the two inputs together
+    mult_gen : for i in 0 to VECTOR_SIZE - 1 generate
+        r_mult_to_add(i) <= std_logic_vector(signed(i_data(i)) * signed(i_weight(i)));
+    end generate;
+
+    -------------------------------------------------------------------------------------
+    -- ADDER TREE
+    -------------------------------------------------------------------------------------
+    -- Instantiate the adder tree
+    adder_tree_inst : adder_tree
+    generic map(
+        N_OPD    => VECTOR_SIZE,
+        BITWIDTH => 2 * BITWIDTH
+    )
+    port map(
+        i_clk  => i_clk,
+        i_rst  => i_rst,
+        i_data => r_mult_to_add,
+        o_data => r_sum
+    );
+
+    -------------------------------------------------------------------------------------
+    -- OUTPUT UPDATE
+    -------------------------------------------------------------------------------------
+    -- Assign the final output data from the first stage of the register
+    o_sum <= r_sum;
+
+end fc_layer_arch;
+
+configuration fc_layer_conf of fc_layer is
+    for fc_layer_arch
+        for all : adder_tree
+            use entity LIB_RTL.adder_tree(adder_tree_arch);
+        end for;
+    end for;
+end configuration fc_layer_conf;
