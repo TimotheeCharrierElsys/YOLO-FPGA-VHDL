678e102297a261d8cd37f44acf3206b10a07febb
TimotheeCharrierElsys <timothee.charrier@elsys-design.com>
2024-05-27 16:01:19 +0200

    feat: add VHDL files for optimized MAC component for alternative conv archtiecture

---
+++ SRC/BENCH/mac/mac_w_mux_tb.vhd
@@ -0,0 +1,116 @@
+-----------------------------------------------------------------------------------
+-- Entity:       mac_w_mux
+-- Description:  This entity implements a Multiply-Accumulate (MAC) unit. It performs
+--               multiplication of two operands followed by an addition with a third operand.
+-- Author:       Timothée Charrier
+-----------------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
+
+library LIB_RTL;
+use LIB_RTL.types_pkg.all;
+
+entity mac_w_mux is
+end entity mac_w_mux;
+
+architecture mac_w_mux_arch of mac_w_mux is
+    -- CONSTANTS
+    constant clock_period : time    := 10 ns; -- Clock period
+    constant BITWIDTH     : integer := 8;     -- Bit width of each operand
+
+    -- SIGNALS
+    signal clock         : std_logic := '0';                        -- Clock signal
+    signal reset_n       : std_logic := '1';                        -- Reset signal, active low
+    signal i_enable      : std_logic := '0';                        -- Enable signal, active low
+    signal i_sel         : std_logic := '1';                        -- Select signal
+    signal i_multiplier1 : std_logic_vector(BITWIDTH - 1 downto 0); -- First multiplier operand
+    signal i_multiplier2 : std_logic_vector(BITWIDTH - 1 downto 0); -- Second multiplier operand
+    signal i_bias        : std_logic_vector(BITWIDTH - 1 downto 0); -- Third operand
+    signal o_result      : std_logic_vector(2 * BITWIDTH - 1 downto 0);-- Output data
+
+    -- COMPONENTS
+    component mac_w_mux
+        generic (
+            BITWIDTH : integer
+        );
+        port (
+            clock         : in std_logic;
+            reset_n       : in std_logic;
+            i_enable      : in std_logic;
+            i_sel         : in std_logic;
+            i_multiplier1 : in std_logic_vector(BITWIDTH - 1 downto 0);
+            i_multiplier2 : in std_logic_vector(BITWIDTH - 1 downto 0);
+            i_bias        : in std_logic_vector(BITWIDTH - 1 downto 0);
+            o_result      : out std_logic_vector(2 * BITWIDTH - 1 downto 0)
+        );
+    end component;
+
+begin
+    -- UNIT UNDER TEST (UUT)
+    UUT : mac_w_mux
+    generic map(
+        BITWIDTH => BITWIDTH
+    )
+    port map(
+        clock         => clock,
+        reset_n       => reset_n,
+        i_enable      => i_enable,
+        i_sel         => i_sel,
+        i_multiplier1 => i_multiplier1,
+        i_multiplier2 => i_multiplier2,
+        i_bias        => i_bias,
+        o_result      => o_result
+    );
+
+    -- Clock generation
+    clock <= not clock after clock_period / 2;
+
+    -- TEST PROCESS
+    stimulus : process
+    begin
+        -- Reset the system
+        reset_n <= '0';
+        wait for 2 * clock_period;
+        reset_n <= '1';
+
+        -- Apply input vectors
+        i_multiplier1 <= std_logic_vector(to_signed(5, BITWIDTH));
+        i_multiplier2 <= std_logic_vector(to_signed(7, BITWIDTH));
+        i_bias        <= std_logic_vector(to_signed(10, BITWIDTH));
+
+        -- MUX select output
+        i_sel <= '0';
+
+        -- Enable the MAC
+        i_enable <= '1';
+
+        -- Check the output with sel = 0
+        wait for (clock_period);
+        assert o_result = std_logic_vector(to_signed(35, 2 * BITWIDTH))
+        report "Test failed: output does not match expected output"
+            severity error;
+
+        i_sel <= '1';
+
+        -- Check the output with sel = 1
+        wait for (clock_period);
+        assert o_result = std_logic_vector(to_signed(45, 2 * BITWIDTH))
+        report "Test failed: output does not match expected output"
+            severity error;
+
+        i_enable <= '0';
+        -- Finish the simulation
+        wait;
+    end process stimulus;
+
+end architecture mac_w_mux_arch;
+
+configuration mac_w_mux_conf of mac_w_mux is
+    for mac_w_mux_arch
+        for UUT : mac_w_mux
+            use entity LIB_RTL.mac_w_mux(mac_w_mux_arch);
+        end for;
+    end for;
+end configuration mac_w_mux_conf;
---
+++ SRC/RTL/mac/mac_w_mux.vhd
@@ -0,0 +1,71 @@
+-----------------------------------------------------------------------------------
+--!     Entity      mac_w_mux
+--!     @brief      This entity implements a Multiply-Accumulate (MAC) unit.
+--!                 It performs multiplication of two operands followed by an addition
+--!                 with a third operand.
+--!     @author     Timothée Charrier
+-----------------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
+
+--! Entity mac_w_mux
+--! This entity implements a Multiply-Accumulate (MAC) unit.
+--! It multiplies two operands and then adds a third operand.
+entity mac_w_mux is
+    generic (
+        BITWIDTH : integer := 8 --! Bit width of each operand
+    );
+    port (
+        clock         : in std_logic;                                   --! Clock signal
+        reset_n       : in std_logic;                                   --! Reset signal, active at low state
+        i_enable      : in std_logic;                                   --! Enable signal, active at low state
+        i_sel         : in std_logic;                                   --! Select signal for the MUX (1 for (bias + mult), 0 for (output + mult))
+        i_multiplier1 : in std_logic_vector(BITWIDTH - 1 downto 0);     --! First multiplication operand
+        i_multiplier2 : in std_logic_vector(BITWIDTH - 1 downto 0);     --! Second multiplication operand
+        i_bias        : in std_logic_vector(BITWIDTH - 1 downto 0);     --! Input addend value
+        o_result      : out std_logic_vector(2 * BITWIDTH - 1 downto 0) --! Output result value
+    );
+end mac_w_mux;
+
+architecture mac_w_mux_arch of mac_w_mux is
+
+    -------------------------------------------------------------------------------------
+    -- SIGNALS
+    -------------------------------------------------------------------------------------
+    signal mux_out : std_logic_vector(2 * BITWIDTH - 1 downto 0); --! MUX output value
+
+begin
+
+    -------------------------------------------------------------------------------------
+    -- PROCESS ASYNC (reset negative)
+    -------------------------------------------------------------------------------------
+    --! Process
+    --! Handles the synchronous and asynchronous operations of the MAC unit.
+    process (clock, reset_n)
+    begin
+        if reset_n = '0' then
+            -- Reset output register to zeros
+            mux_out <= (others => '0');
+        elsif rising_edge(clock) then
+            if (i_enable = '1') then
+
+                -- MUX update based on i_sel signal
+                --      If i_sel is '0', mux_out takes the value of intermediate mac result + multiplication result             
+                --      If i_sel is '1', mux_out takes the resized value of i_bias + multiplication result
+                if i_sel = '0' then
+                    mux_out <= std_logic_vector(signed(mux_out) + signed(i_multiplier1) * signed(i_multiplier2)); --! Add the output value with the multiplciation
+                else
+                    mux_out <= std_logic_vector(signed(i_bias) + signed(i_multiplier1) * signed(i_multiplier2)); --! Add Bias with the multitplication
+                end if;
+            end if;
+        end if;
+    end process;
+
+    -------------------------------------------------------------------------------------
+    -- OUTPUT ASSIGNMENT
+    -------------------------------------------------------------------------------------
+    o_result <= mux_out;
+
+end mac_w_mux_arch;
