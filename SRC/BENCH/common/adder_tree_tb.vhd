69be7f8c3570a151f6e21a36f54b4d6c32b04586
TimotheeCharrierElsys <timothee.charrier@elsys-design.com>
2024-06-05 10:54:32 +0200

    feat: add unpipelined archtiecture

--- SRC/BENCH/common/adder_tree_tb.vhd
+++ SRC/BENCH/common/adder_tree_tb.vhd
@@ -1,5 +1,5 @@
 -----------------------------------------------------------------------------------
---!     @Testbench    binary_adder_tree_tb
+--!     @Testbench    adder_tree_tb
 --!     @brief        This testbench verifies the functionality of the pipelined MOA.
 --!     @details      It initializes the inputs, applies test vectors, and checks the outputs.
 --!     @auth         Timothée Charrier
@@ -10,9 +10,9 @@
 --!   "signal": [
 --!     {"name": "clock",    "wave": "N.....", "period": 1},
 --!     {"name": "reset_n",    "wave": "10...."},
---!     {"name": "i_enable", "wave": "01...."},
---!     {"name": "i_data",   "wave": "x3....", "data": ["{8,7,6,5,4,3,2,1}"]},
---!     {"name": "o_data",   "wave": "x5..4.", "data": ["0","36"]}
+--!     {"name": "i_sys_enable", "wave": "01...."},
+--!     {"name": "i_data",   "wave": "x3....", "data": ["{6,5,4,3,2,1}"]},
+--!     {"name": "o_data",   "wave": "x5..4.", "data": ["0","21"]}
 --!   ],
 --!   "config": { "hscale": 2 }
 --! }
@@ -24,38 +24,40 @@ use IEEE.NUMERIC_STD.all;
 library LIB_RTL;
 use LIB_RTL.types_pkg.all;
 
-entity binary_adder_tree_tb is
+entity adder_tree_tb is
 end entity;
 
-architecture binary_adder_tree_tb_arch of binary_adder_tree_tb is
+architecture adder_tree_tb_arch of adder_tree_tb is
     -------------------------------------------------------------------------------------
     -- CONSTANTS
     -------------------------------------------------------------------------------------
     constant i_clk_period : time    := 10 ns; --! Clock period
-    constant N_OPD        : integer := 8;     --! Number of operands
+    constant N_OPD        : integer := 6;     --! Number of operands
     constant BITWIDTH     : integer := 8;     --! Bit BITWIDTH of each operand
 
     -------------------------------------------------------------------------------------
     -- SIGNALS
     -------------------------------------------------------------------------------------
-    signal clock   : std_logic := '0';                                 --! Clock signal
-    signal reset_n : std_logic := '1';                                 --! Reset signal
-    signal i_data  : t_vec(N_OPD - 1 downto 0)(BITWIDTH - 1 downto 0); --! Input data vector
-    signal o_data  : std_logic_vector(BITWIDTH - 1 downto 0);          --! Output data
+    signal clock        : std_logic := '0';                                 --! Clock signal
+    signal reset_n      : std_logic := '1';                                 --! Reset signal
+    signal i_sys_enable : std_logic := '0';                                 --! Reset signal, active at low state
+    signal i_data       : t_vec(N_OPD - 1 downto 0)(BITWIDTH - 1 downto 0); --! Input data vector
+    signal o_data       : std_logic_vector(BITWIDTH - 1 downto 0);          --! Output data
 
     -------------------------------------------------------------------------------------
     -- COMPONENTS
     -------------------------------------------------------------------------------------
-    component binary_adder_tree
+    component adder_tree
         generic (
             N_OPD    : integer;
             BITWIDTH : integer
         );
         port (
-            clock   : in std_logic;
-            reset_n : in std_logic;
-            i_data  : in t_vec(N_OPD - 1 downto 0)(BITWIDTH - 1 downto 0);
-            o_data  : out std_logic_vector(BITWIDTH - 1 downto 0)
+            clock        : in std_logic;
+            reset_n      : in std_logic;
+            i_sys_enable : in std_logic;
+            i_data       : in t_vec(N_OPD - 1 downto 0)(BITWIDTH - 1 downto 0);
+            o_data       : out std_logic_vector(BITWIDTH - 1 downto 0)
         );
     end component;
 
@@ -63,16 +65,17 @@ begin
     -------------------------------------------------------------------------------------
     -- UNIT UNDER TEST (UUT)
     -------------------------------------------------------------------------------------
-    UUT : binary_adder_tree
+    UUT : adder_tree
     generic map(
         N_OPD    => N_OPD,
         BITWIDTH => BITWIDTH
     )
     port map(
-        clock   => clock,
-        reset_n => reset_n,
-        i_data  => i_data,
-        o_data  => o_data
+        clock        => clock,
+        reset_n      => reset_n,
+        i_sys_enable => i_sys_enable,
+        i_data       => i_data,
+        o_data       => o_data
     );
 
     -- Clock generation
@@ -88,6 +91,9 @@ begin
         wait for 2 * i_clk_period;
         reset_n <= '1';
 
+        -- Enable
+        i_sys_enable <= '1';
+
         -- Apply input vectors
         i_data(0) <= std_logic_vector(to_unsigned(1, BITWIDTH));
         i_data(1) <= std_logic_vector(to_unsigned(2, BITWIDTH));
@@ -95,14 +101,14 @@ begin
         i_data(3) <= std_logic_vector(to_unsigned(4, BITWIDTH));
         i_data(4) <= std_logic_vector(to_unsigned(5, BITWIDTH));
         i_data(5) <= std_logic_vector(to_unsigned(6, BITWIDTH));
-        i_data(6) <= std_logic_vector(to_unsigned(7, BITWIDTH));
-        i_data(7) <= std_logic_vector(to_unsigned(8, BITWIDTH));
+        -- i_data(6) <= std_logic_vector(to_unsigned(7, BITWIDTH));
+        -- i_data(7) <= std_logic_vector(to_unsigned(8, BITWIDTH));
 
         -- Wait for enough time to allow the pipeline to process the inputs
         wait for (N_OPD * i_clk_period);
 
         -- Check the output
-        assert o_data = std_logic_vector(to_unsigned(36, BITWIDTH))
+        assert o_data = std_logic_vector(to_unsigned(21, BITWIDTH))
         report "Test failed: output does not match expected sum"
             severity error;
 
@@ -112,10 +118,10 @@ begin
 
 end architecture;
 
-configuration binary_adder_tree_tb_conf of binary_adder_tree_tb is
-    for binary_adder_tree_tb_arch
-        for UUT : binary_adder_tree
-            use entity LIB_RTL.binary_adder_tree(binary_adder_tree_arch);
+configuration adder_tree_tb_conf of adder_tree_tb is
+    for adder_tree_tb_arch
+        for UUT : adder_tree
+            use entity LIB_RTL.adder_tree(adder_tree_pipelined_arch);
         end for;
     end for;
-end configuration binary_adder_tree_tb_conf;
+end configuration adder_tree_tb_conf;
--- SRC/RTL/common/adder_tree.vhd
+++ SRC/RTL/common/adder_tree.vhd
@@ -22,15 +22,15 @@ entity adder_tree is
         BITWIDTH : integer := 8   --! Bit width of each operand
     );
     port (
-        clock    : in std_logic;                                        --! Clock signal
-        reset_n  : in std_logic;                                        --! Reset signal, active at low state
-        i_enable : in std_logic;                                        --! Reset signal, active at low state
-        i_data   : in t_vec(N_OPD - 1 downto 0)(BITWIDTH - 1 downto 0); --! Input data vector
-        o_data   : out std_logic_vector(BITWIDTH - 1 downto 0)          --! Output data
+        clock        : in std_logic;                                        --! Clock signal
+        reset_n      : in std_logic;                                        --! Reset signal, active at low state
+        i_sys_enable : in std_logic;                                        --! Reset signal, active at low state
+        i_data       : in t_vec(N_OPD - 1 downto 0)(BITWIDTH - 1 downto 0); --! Input data vector
+        o_data       : out std_logic_vector(BITWIDTH - 1 downto 0)          --! Output data
     );
 end adder_tree;
 
-architecture adder_tree_arch of adder_tree is
+architecture adder_tree_pipelined_arch of adder_tree is
 
     -------------------------------------------------------------------------------------
     -- CONSTANTS
@@ -84,7 +84,7 @@ begin
                 r_reg(i) <= (others => (others => '0'));
             end loop;
         elsif rising_edge(clock) then
-            if i_enable = '1' then
+            if i_sys_enable = '1' then
                 r_reg <= r_next; -- Transfer next state to current state on rising edge of the clock
             end if;
         end if;
@@ -95,5 +95,72 @@ begin
     -------------------------------------------------------------------------------------
     -- Assign the final output data from the first stage of the register
     o_data <= r_reg(0)(0);
+
+end adder_tree_pipelined_arch;
+
+architecture adder_tree_arch of adder_tree is
+
+    -------------------------------------------------------------------------------------
+    -- CONSTANTS
+    -------------------------------------------------------------------------------------
+    constant N_STAGES : integer := integer(ceil(log2(real(N_OPD)))); --! Number of stages required to complete the addition process.
+
+    -------------------------------------------------------------------------------------
+    -- SIGNALS
+    -------------------------------------------------------------------------------------
+    signal intermediate_sums : t_mat(0 to N_STAGES)(0 to (2 ** N_STAGES) - 1)(BITWIDTH - 1 downto 0);
+    signal r_output          : std_logic_vector(BITWIDTH - 1 downto 0);
+
+begin
+
+    -------------------------------------------------------------------------------------
+    -- INPUT GENERATION
+    -------------------------------------------------------------------------------------
+    -- This generate block assigns the input data to the appropriate stage of the adder tree.
+    input_gen : for i in 0 to N_OPD - 1 generate
+        intermediate_sums(N_STAGES)(i) <= i_data(i);
+    end generate;
+
+    -------------------------------------------------------------------------------------
+    -- PADDING GENERATION
+    -------------------------------------------------------------------------------------
+    -- This generate block pads the remaining positions with zeros if the number of operands is less than 2^N_STAGES.
+    pading_gen : if N_OPD < 2 ** N_STAGES generate
+        intermediate_sums(N_STAGES)(N_OPD to (2 ** N_STAGES) - 1) <= (others => (others => '0'));
+    end generate;
+
+    -------------------------------------------------------------------------------------
+    -- STAGE GENERATION
+    -------------------------------------------------------------------------------------
+    -- This generate block creates the tree structure of the adder, reducing the number of operands by half in each stage.
+    stage_gen : for i in (N_STAGES - 1) downto 0 generate
+        row_gen : for j in 0 to (2 ** i - 1) generate
+            intermediate_sums(i)(j) <= std_logic_vector(
+            signed(intermediate_sums(i + 1)(2 * j)) + signed(intermediate_sums(i + 1)(2 * j + 1))
+            );
+        end generate;
+    end generate;
+
+    -------------------------------------------------------------------------------------
+    -- PROCESS ASYNC (reset negative)
+    -------------------------------------------------------------------------------------
+    --! Process
+    --! Handles the synchronous and asynchronous operations of the pipelined adder.
+    process (clock, reset_n)
+    begin
+        if reset_n = '0' then
+            -- Initialize the register with zeros on reset
+            for i in 0 to N_STAGES loop
+                r_output <= (others => '0');
+            end loop;
+        elsif rising_edge(clock) then
+            if i_sys_enable = '1' then
+                r_output <= intermediate_sums(0)(0);
+            end if;
+        end if;
+    end process;
+
+    -- Assign the final output data from the first stage of the intermediate sums
+    o_data <= r_output;
 
 end adder_tree_arch;
