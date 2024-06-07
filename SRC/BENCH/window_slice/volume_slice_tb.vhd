64cdf5bd8f84cce55aa31df30dbec326d60720f2
TimotheeCharrierElsys <timothee.charrier@elsys-design.com>
2024-06-07 10:29:32 +0200

    feat: improve testbench adding more cases

--- SRC/BENCH/window_slice/volume_slice_tb.vhd
+++ SRC/BENCH/window_slice/volume_slice_tb.vhd
@@ -20,39 +20,49 @@ architecture volume_slice_tb_arch of volume_slice_tb is
     -- CONSTANTS
     -------------------------------------------------------------------------------------
     constant i_clk_period   : time    := 10 ns; --! Clock period
-    constant WAIT_COUNT     : integer := 5;
+    constant NUM_REPEATS    : integer := 9;
+    constant KERNEL_SIZE    : integer := 3;
     constant BITWIDTH       : integer := 8;
     constant INPUT_SIZE     : integer := 5;
     constant CHANNEL_NUMBER : integer := 3;
     constant OUTPUT_SIZE    : integer := 3;
+    constant STRIDE         : integer := 1;
 
     -------------------------------------------------------------------------------------
     -- SIGNALS
     -------------------------------------------------------------------------------------
-    signal i_data            : t_volume(CHANNEL_NUMBER - 1 downto 0)(INPUT_SIZE - 1 downto 0)(INPUT_SIZE - 1 downto 0)(BITWIDTH - 1 downto 0);
-    signal i_row_index_start : std_logic_vector(INPUT_SIZE - 1 downto 0) := std_logic_vector(to_unsigned(1, INPUT_SIZE));
-    signal i_row_index_end   : std_logic_vector(INPUT_SIZE - 1 downto 0) := std_logic_vector(to_unsigned(3, INPUT_SIZE));
-    signal i_col_index_start : std_logic_vector(INPUT_SIZE - 1 downto 0) := std_logic_vector(to_unsigned(1, INPUT_SIZE));
-    signal i_col_index_end   : std_logic_vector(INPUT_SIZE - 1 downto 0) := std_logic_vector(to_unsigned(3, INPUT_SIZE));
-    signal o_data            : t_volume(CHANNEL_NUMBER - 1 downto 0)(OUTPUT_SIZE - 1 downto 0)(OUTPUT_SIZE - 1 downto 0)(BITWIDTH - 1 downto 0);
+    signal clock                   : std_logic := '0';
+    signal reset_n                 : std_logic := '0';
+    signal i_sys_enable            : std_logic := '0';
+    signal i_data                  : t_volume(CHANNEL_NUMBER - 1 downto 0)(INPUT_SIZE - 1 downto 0)(INPUT_SIZE - 1 downto 0)(BITWIDTH - 1 downto 0);
+    signal i_data_valid            : std_logic := '0';
+    signal i_last_computation_done : std_logic := '0';
+    signal o_data                  : t_volume(CHANNEL_NUMBER - 1 downto 0)(OUTPUT_SIZE - 1 downto 0)(OUTPUT_SIZE - 1 downto 0)(BITWIDTH - 1 downto 0);
+    signal o_done                  : std_logic;
+    signal o_computation_start     : std_logic;
 
     -------------------------------------------------------------------------------------
     -- COMPONENTS
     -------------------------------------------------------------------------------------
     component volume_slice
         generic (
-            BITWIDTH       : integer := 8; --! Bit width of each operand
-            INPUT_SIZE     : integer := 5; --! Input size
-            CHANNEL_NUMBER : integer := 3; --! Number of channels in the image
-            OUTPUT_SIZE    : integer := 3  --! Output size
+            BITWIDTH       : integer;
+            INPUT_SIZE     : integer;
+            CHANNEL_NUMBER : integer;
+            KERNEL_SIZE    : integer;
+            STRIDE         : integer;
+            OUTPUT_SIZE    : integer
         );
         port (
-            i_data            : in t_volume(CHANNEL_NUMBER - 1 downto 0)(INPUT_SIZE - 1 downto 0)(INPUT_SIZE - 1 downto 0)(BITWIDTH - 1 downto 0);   --! Input matrix
-            i_row_index_start : in std_logic_vector(INPUT_SIZE - 1 downto 0);                                                                        --! First dimension (row) starting index
-            i_row_index_end   : in std_logic_vector(INPUT_SIZE - 1 downto 0);                                                                        --! First dimension (row) ending index
-            i_col_index_start : in std_logic_vector(INPUT_SIZE - 1 downto 0);                                                                        --! Second dimension (col) starting index
-            i_col_index_end   : in std_logic_vector(INPUT_SIZE - 1 downto 0);                                                                        --! Second dimension (col) ending index
-            o_data            : out t_volume(CHANNEL_NUMBER - 1 downto 0)(OUTPUT_SIZE - 1 downto 0)(OUTPUT_SIZE - 1 downto 0)(BITWIDTH - 1 downto 0) --! Output sliced matrix
+            clock                   : in std_logic;
+            reset_n                 : in std_logic;
+            i_sys_enable            : in std_logic;
+            i_data                  : in t_volume(CHANNEL_NUMBER - 1 downto 0)(INPUT_SIZE - 1 downto 0)(INPUT_SIZE - 1 downto 0)(BITWIDTH - 1 downto 0);
+            i_data_valid            : in std_logic;
+            i_last_computation_done : in std_logic;
+            o_data                  : out t_volume(CHANNEL_NUMBER - 1 downto 0)(OUTPUT_SIZE - 1 downto 0)(OUTPUT_SIZE - 1 downto 0)(BITWIDTH - 1 downto 0);
+            o_done                  : out std_logic;
+            o_computation_start     : out std_logic
         );
     end component;
 
@@ -62,18 +72,26 @@ begin
     -------------------------------------------------------------------------------------
     UUT : volume_slice
     generic map(
-        BITWIDTH    => BITWIDTH,
-        INPUT_SIZE  => INPUT_SIZE,
-        OUTPUT_SIZE => OUTPUT_SIZE
+        BITWIDTH       => BITWIDTH,
+        INPUT_SIZE     => INPUT_SIZE,
+        CHANNEL_NUMBER => CHANNEL_NUMBER,
+        KERNEL_SIZE    => KERNEL_SIZE,
+        STRIDE         => STRIDE,
+        OUTPUT_SIZE    => OUTPUT_SIZE
     )
     port map(
-        i_data            => i_data,
-        i_row_index_start => i_row_index_start,
-        i_row_index_end   => i_row_index_end,
-        i_col_index_start => i_col_index_start,
-        i_col_index_end   => i_col_index_end,
-        o_data            => o_data
+        clock                   => clock,
+        reset_n                 => reset_n,
+        i_sys_enable            => i_sys_enable,
+        i_data                  => i_data,
+        i_data_valid            => i_data_valid,
+        i_last_computation_done => i_last_computation_done,
+        o_data                  => o_data,
+        o_done                  => o_done,
+        o_computation_start     => o_computation_start
     );
+    -- Clock generation
+    clock <= not clock after i_clk_period / 2;
 
     i_data(0) <= (
     (std_logic_vector(to_signed(0, BITWIDTH)), std_logic_vector(to_signed(0, BITWIDTH)), std_logic_vector(to_signed(0, BITWIDTH)), std_logic_vector(to_signed(0, BITWIDTH)), std_logic_vector(to_signed(0, BITWIDTH))),
@@ -98,6 +116,45 @@ begin
     (std_logic_vector(to_signed(0, BITWIDTH)), std_logic_vector(to_signed(16, BITWIDTH)), std_logic_vector(to_signed(17, BITWIDTH)), std_logic_vector(to_signed(18, BITWIDTH)), std_logic_vector(to_signed(0, BITWIDTH))),
     (std_logic_vector(to_signed(0, BITWIDTH)), std_logic_vector(to_signed(0, BITWIDTH)), std_logic_vector(to_signed(0, BITWIDTH)), std_logic_vector(to_signed(0, BITWIDTH)), std_logic_vector(to_signed(0, BITWIDTH)))
     );
+
+    -------------------------------------------------------------------------------------
+    -- TEST PROCESS
+    -------------------------------------------------------------------------------------
+    stimulus : process
+    begin
+        -- Reset the system
+        reset_n <= '0';
+        wait for i_clk_period;
+        reset_n      <= '1';
+        i_sys_enable <= '1';
+        i_data_valid <= '1';
+        wait for i_clk_period;
+        i_data_valid <= '0';
+        wait for 2 * i_clk_period;
+
+        for i in 1 to NUM_REPEATS loop
+            i_last_computation_done <= '1';
+            wait for i_clk_period;
+            i_last_computation_done <= '0';
+            wait for 2 * i_clk_period;
+        end loop;
+
+        wait for 10 * i_clk_period;
+
+        i_data_valid <= '1';
+        wait for i_clk_period;
+        i_data_valid <= '0';
+        wait for 2 * i_clk_period;
+
+        for i in 1 to NUM_REPEATS loop
+            i_last_computation_done <= '1';
+            wait for i_clk_period;
+            i_last_computation_done <= '0';
+            wait for 2 * i_clk_period;
+        end loop;
+
+        wait for 10 * i_clk_period;
+    end process stimulus;
 
 end architecture;
 
