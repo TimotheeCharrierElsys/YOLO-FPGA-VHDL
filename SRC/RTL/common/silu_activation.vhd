7a08917f0dea609f7965db418974886a44af7359
TimotheeCharrierElsys <timothee.charrier@elsys-design.com>
2024-06-18 16:44:29 +0200

    feat: add first draft of silu activation function

--- SRC/RTL/common/silu_activation.vhd
+++ SRC/RTL/common/silu_activation.vhd
@@ -1,11 +1,22 @@
+-----------------------------------------------------------------------------------
+--!     @file       silu_activation
+--!     @brief      This entity implements a scaled silu activation function 
+--!                 It uses hardswish approxmation fucntion.
+--!                 See https://arxiv.org/pdf/1905.02244 for more details
+--!     @author     Timothée Charrier
+-----------------------------------------------------------------------------------
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 use IEEE.NUMERIC_STD.all;
+use IEEE.MATH_REAL.all;
 
+--! Entity silu_activation
+--! This entity implements an approximated silu activation function.
 entity silu_activation is
     generic (
-        BITWIDTH     : integer := 16;     --! Bit width of each operand
-        SCALE_FACTOR : integer := 2 ** 10 --! Scale factor for integer computation
+        BITWIDTH            : integer := 16; --! Bit width of each operand
+        SCALE_FACTOR_POWER2 : integer := 10  --! Scale factor for integer computation power (eg. 10 -> 2**10)
     );
     port (
         clock        : in std_logic;                                   --! Clock signal
@@ -18,39 +29,65 @@ end silu_activation;
 
 architecture silu_activation_arch of silu_activation is
 
-    constant a : integer := 2 ** 5;
-    constant b : integer := 2 ** 8;
-    constant c : integer := 512;
+    -------------------------------------------------------------------------------------
+    -- CONSTANT
+    -------------------------------------------------------------------------------------
+    constant SCALE_FACTOR   : integer := 2 ** SCALE_FACTOR_POWER2;
+    constant THRESHOLD_NEG  : integer := - 3 * SCALE_FACTOR;
+    constant THRESHOLD_POS  : integer := 3 * SCALE_FACTOR;
+    constant DIVISION_VALUE : integer := integer(ceil(log2(real(SCALE_FACTOR/6))));
 
-    signal i_data_int : integer;
+    -------------------------------------------------------------------------------------
+    -- SIGNAL
+    -------------------------------------------------------------------------------------
+    signal i_data_signed : signed(BITWIDTH - 1 downto 0);
 
 begin
 
-    i_data_int <= to_integer(unsigned(i_data));
+    -- Input conversion for computation
+    i_data_signed <= signed(i_data);
 
     -------------------------------------------------------------------------------------
-    -- PROCESS
+    -- GENERATE PROCESS
     -------------------------------------------------------------------------------------
-    silu_computation_process : process (clock, reset_n)
-        variable polynomial_computation : integer;
+    --! Process
+    --! Handles the computation of the activation function
+    process (clock, reset_n)
+        variable hardswish_add  : signed(BITWIDTH - 1 downto 0);
+        variable hardswish_mult : signed(2 * BITWIDTH - 1 downto 0);
     begin
         if reset_n = '0' then
-            o_data <= (others => '0');
+            hardswish_mult := (others => '0');
+            hardswish_add  := (others => '0');
+            o_data <= (others         => '0');
 
         elsif rising_edge(clock) then
             if i_sys_enable = '1' then
-                -- Computation
-                polynomial_computation := (a * i_data_int * i_data_int + b * i_data_int + c);
 
-                -- Output update
-                if i_data_int >= to_unsigned(SCALE_FACTOR, BITWIDTH) then
-                    o_data <= std_logic_vector(to_unsigned(SCALE_FACTOR, 2 * BITWIDTH));
+                if i_data_signed < THRESHOLD_NEG then
+                    -- Test if x < -3 scaled
+                    hardswish_mult := (others => '0');
+                elsif i_data_signed < THRESHOLD_POS then
+                    -- Test if x > -3  and x < 3 scaled
+
+                    -- Compute the addition
+                    hardswish_add := i_data_signed + to_signed(THRESHOLD_POS, BITWIDTH);
+
+                    -- Compute x * (x + 3) scaled
+                    hardswish_mult := resize(hardswish_add * i_data_signed, 2 * BITWIDTH);
+
+                    -- Compute the division using division to multiplication method (multiplu then shift)
+                    hardswish_mult := resize(hardswish_mult * to_signed(DIVISION_VALUE, 2 * BITWIDTH), 2 * BITWIDTH);
+                    hardswish_mult := shift_right(hardswish_mult, SCALE_FACTOR_POWER2);
+
                 else
-                    o_data <= std_logic_vector(to_unsigned(polynomial_computation, 2 * BITWIDTH));
+                    -- Test if x > 3
+                    hardswish_mult := resize(i_data_signed, 2 * BITWIDTH);
                 end if;
 
+                -- Output update
+                o_data <= std_logic_vector(hardswish_mult);
             end if;
         end if;
-    end process silu_computation_process;
-
+    end process;
 end architecture;
