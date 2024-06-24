617db0713a3ef67c38976318dfe5af4767687611
TimotheeCharrierElsys <timothee.charrier@elsys-design.com>
2024-06-24 11:47:35 +0200

    chore: update testbench

--- SRC/BENCH/common/silu_activation_tb.vhd
+++ SRC/BENCH/common/silu_activation_tb.vhd
@@ -3,10 +3,13 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+library LIB_RTL;
+use LIB_RTL.types_pkg.all;
+
 entity silu_activation_tb is
 end;
 
-architecture bench of silu_activation_tb is
+architecture silu_activation_tb_arch of silu_activation_tb is
     -- Clock period
     constant clock_period : time := 5 ns;
     -- Generics
@@ -19,21 +22,36 @@ architecture bench of silu_activation_tb is
     signal i_sys_enable : std_logic := '0';
     signal i_data       : std_logic_vector(BITWIDTH - 1 downto 0);
     signal o_data       : std_logic_vector(2 * BITWIDTH - 1 downto 0);
+
+    component silu_activation
+        generic (
+            BITWIDTH                         : integer;
+            SCALE_FACTOR_POWER_OF_2          : integer;
+            DIVISION_SCALE_FACTOR_POWER_OF_2 : integer
+        );
+        port (
+            clock        : in std_logic;
+            reset_n      : in std_logic;
+            i_sys_enable : in std_logic;
+            i_data       : in std_logic_vector(BITWIDTH - 1 downto 0);
+            o_data       : out std_logic_vector(2 * BITWIDTH - 1 downto 0)
+        );
+    end component;
 begin
 
-    silu_activation_inst : entity work.silu_activation
-        generic map(
-            BITWIDTH                         => BITWIDTH,
-            SCALE_FACTOR_POWER_OF_2          => SCALE_FACTOR_POWER_OF_2,
-            DIVISION_SCALE_FACTOR_POWER_OF_2 => DIVISION_SCALE_FACTOR_POWER_OF_2
-        )
-        port map(
-            clock        => clock,
-            reset_n      => reset_n,
-            i_sys_enable => i_sys_enable,
-            i_data       => i_data,
-            o_data       => o_data
-        );
+    UUT : silu_activation
+    generic map(
+        BITWIDTH                         => BITWIDTH,
+        SCALE_FACTOR_POWER_OF_2          => SCALE_FACTOR_POWER_OF_2,
+        DIVISION_SCALE_FACTOR_POWER_OF_2 => DIVISION_SCALE_FACTOR_POWER_OF_2
+    )
+    port map(
+        clock        => clock,
+        reset_n      => reset_n,
+        i_sys_enable => i_sys_enable,
+        i_data       => i_data,
+        o_data       => o_data
+    );
 
     clock <= not clock after clock_period/2;
 
@@ -60,3 +78,11 @@ begin
     end process stimulus;
 
 end;
+
+configuration silu_activation_tb_conf of silu_activation_tb is
+    for silu_activation_tb_arch
+        for UUT : silu_activation
+            use entity LIB_RTL.silu_activation(silu_activation_arch);
+        end for;
+    end for;
+end configuration silu_activation_tb_conf;
