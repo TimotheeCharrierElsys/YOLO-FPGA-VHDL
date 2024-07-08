f34010d98616746e92d15a76459ff3929d48c1d0
TimotheeCharrierElsys <timothee.charrier@elsys-design.com>
2024-07-08 16:43:14 +0200

    feat: update testbenches for Vivado

--- SRC/BENCH/common/square_root_tb.vhd
+++ SRC/BENCH/common/square_root_tb.vhd
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------------------
---!     @Testbench    silu_activation_tb
---!     @brief        This testbench verifies the functionality of the silu_activation.
+--!     @Testbench    square_root_tb
+--!     @brief        This testbench verifies the functionality of the square_root.
 --!     @details      It initializes the inputs, applies test vectors, and checks the outputs.
 --!     @auth         Timothée Charrier
 -----------------------------------------------------------------------------------
@@ -12,17 +12,15 @@ use ieee.numeric_std.all;
 library LIB_RTL;
 use LIB_RTL.types_pkg.all;
 
-entity silu_activation_tb is
+entity square_root_tb is
 end;
 
-architecture silu_activation_tb_arch of silu_activation_tb is
+architecture square_root_tb_arch of square_root_tb is
     -------------------------------------------------------------------------------------
     -- CONSTANTS
     -------------------------------------------------------------------------------------
-    constant clock_period                     : time    := 5 ns;
-    constant BITWIDTH                         : integer := 16;
-    constant SCALE_FACTOR_POWER_OF_2          : integer := 10;
-    constant DIVISION_SCALE_FACTOR_POWER_OF_2 : integer := 10;
+    constant clock_period : time    := 5 ns;
+    constant BITWIDTH     : integer := 16;
 
     -------------------------------------------------------------------------------------
     -- SIGNALS
@@ -31,35 +29,32 @@ architecture silu_activation_tb_arch of silu_activation_tb is
     signal reset_n      : std_logic := '0';
     signal i_sys_enable : std_logic := '0';
     signal i_data       : std_logic_vector(BITWIDTH - 1 downto 0);
-    signal o_data       : std_logic_vector(2 * BITWIDTH - 1 downto 0);
+    signal o_data       : std_logic_vector(BITWIDTH/2 - 1 downto 0);
 
     -------------------------------------------------------------------------------------
     -- COMPONENTS
     -------------------------------------------------------------------------------------
-    component silu_activation
+    component square_root
         generic (
-            BITWIDTH                         : integer;
-            SCALE_FACTOR_POWER_OF_2          : integer;
-            DIVISION_SCALE_FACTOR_POWER_OF_2 : integer
+            BITWIDTH : integer
         );
         port (
             clock        : in std_logic;
             reset_n      : in std_logic;
             i_sys_enable : in std_logic;
-            i_data       : in std_logic_vector(BITWIDTH - 1 downto 0);
-            o_data       : out std_logic_vector(2 * BITWIDTH - 1 downto 0)
+            i_data       : in std_logic_vector (BITWIDTH - 1 downto 0);
+            o_data       : out std_logic_vector (BITWIDTH/2 - 1 downto 0)
         );
     end component;
+
 begin
 
     -------------------------------------------------------------------------------------
     -- UNIT UNDER TEST (UUT)
     -------------------------------------------------------------------------------------
-    UUT : silu_activation
+    UUT : square_root
     generic map(
-        BITWIDTH                         => BITWIDTH,
-        SCALE_FACTOR_POWER_OF_2          => SCALE_FACTOR_POWER_OF_2,
-        DIVISION_SCALE_FACTOR_POWER_OF_2 => DIVISION_SCALE_FACTOR_POWER_OF_2
+        BITWIDTH => BITWIDTH
     )
     port map(
         clock        => clock,
@@ -86,7 +81,7 @@ begin
         i_sys_enable <= '1';
         wait for clock_period/2;
 
-        for i in -7000 to 7000 loop
+        for i in 0 to 65534 loop
             i_data <= std_logic_vector(to_signed(i, BITWIDTH));
             wait for clock_period;
         end loop;
@@ -96,10 +91,10 @@ begin
 
 end;
 
-configuration silu_activation_tb_conf of silu_activation_tb is
-    for silu_activation_tb_arch
-        for UUT : silu_activation
-            use entity LIB_RTL.silu_activation(silu_activation_arch);
+configuration square_root_tb_conf of square_root_tb is
+    for square_root_tb_arch
+        for UUT : square_root
+            use entity LIB_RTL.square_root(square_root_arch);
         end for;
     end for;
-end configuration silu_activation_tb_conf;
+end configuration square_root_tb_conf;
---
+++ SRC/BENCH/mac/accumulative_mac_tb.vhd
@@ -0,0 +1,121 @@
+-----------------------------------------------------------------------------------
+--!     @Testbench    accumulative_mac
+--!     @brief        This testbench verifies the functionality of the accumulative mac
+--!     @details      It initializes the inputs, applies test vectors, and checks the outputs.
+--!     @auth         Timothée Charrier
+-----------------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
+
+library LIB_RTL;
+use LIB_RTL.types_pkg.all;
+
+entity accumulative_mac is
+end entity;
+
+architecture accumulative_mac_arch of accumulative_mac is
+    -------------------------------------------------------------------------------------
+    -- CONSTANTS
+    -------------------------------------------------------------------------------------
+    constant i_clk_period : time    := 10 ns; --! Clock period
+    constant WAIT_COUNT   : integer := 8;     --! Number clock tics to wait
+    constant BITWIDTH     : integer := 8;     --! Bit BITWIDTH of each operand
+
+    -------------------------------------------------------------------------------------
+    -- SIGNALS
+    -------------------------------------------------------------------------------------
+    signal clock         : std_logic := '0';                            --! Clock signal
+    signal reset_n       : std_logic := '1';                            --! Reset signal, active at low state
+    signal i_sys_enable  : std_logic := '0';                            --! Enable signal, active at high state
+    signal i_clear       : std_logic := '0';                            --! Clear signal, active high
+    signal i_multiplier1 : std_logic_vector(BITWIDTH - 1 downto 0);     --! First mult operand
+    signal i_multiplier2 : std_logic_vector(BITWIDTH - 1 downto 0);     --! Second mult operand
+    signal o_result      : std_logic_vector(2 * BITWIDTH - 1 downto 0); --! Output data
+
+    -------------------------------------------------------------------------------------
+    -- COMPONENTS
+    -------------------------------------------------------------------------------------
+    component accumulative_mac
+        generic (
+            BITWIDTH : integer
+        );
+        port (
+            clock         : in std_logic;
+            reset_n       : in std_logic;
+            i_sys_enable  : in std_logic;
+            i_clear       : in std_logic;
+            i_multiplier1 : in std_logic_vector(BITWIDTH - 1 downto 0);
+            i_multiplier2 : in std_logic_vector(BITWIDTH - 1 downto 0);
+            o_result      : out std_logic_vector(2 * BITWIDTH - 1 downto 0)
+        );
+    end component;
+
+begin
+    -------------------------------------------------------------------------------------
+    -- UNIT UNDER TEST (UUT)
+    -------------------------------------------------------------------------------------
+    UUT : accumulative_mac
+    generic map(
+        BITWIDTH => BITWIDTH
+    )
+    port map(
+        clock         => clock,
+        reset_n       => reset_n,
+        i_sys_enable  => i_sys_enable,
+        i_clear       => i_clear,
+        i_multiplier1 => i_multiplier1,
+        i_multiplier2 => i_multiplier2,
+        o_result      => o_result
+    );
+    -- Clock generation
+    clock <= not clock after i_clk_period / 2;
+
+    -------------------------------------------------------------------------------------
+    -- TEST PROCESS
+    -------------------------------------------------------------------------------------
+    stimulus : process
+    begin
+        -- Reset the system
+        reset_n <= '0';
+        wait for 2 * i_clk_period;
+        reset_n <= '1';
+
+        -- Enable the mac unit
+        i_sys_enable <= '1';
+
+        -- Apply input vectors
+        i_multiplier1 <= std_logic_vector(to_signed(5, BITWIDTH));
+        i_multiplier2 <= std_logic_vector(to_signed(7, BITWIDTH));
+
+        -- Wait for enough time to allow the pipeline to process the inputs
+        wait for (WAIT_COUNT * i_clk_period);
+
+        -- Check the output
+        assert o_result = std_logic_vector(to_signed(45, 2 * BITWIDTH))
+        report "Test failed: output does not match expected output"
+            severity error;
+
+        i_clear <= '1';
+        wait for i_clk_period;
+        i_clear <= '0';
+        wait for i_clk_period;
+
+        assert o_result = std_logic_vector(to_signed(0, 2 * BITWIDTH))
+        report "Test failed: output not cleared correctly"
+            severity error;
+
+        -- Finish the simulation
+        wait;
+    end process stimulus;
+
+end architecture;
+
+configuration accumulative_mac_conf of accumulative_mac is
+    for accumulative_mac_arch
+        for UUT : accumulative_mac
+            use entity LIB_RTL.accumulative_mac(accumulative_mac_arch);
+        end for;
+    end for;
+end configuration accumulative_mac_conf;
