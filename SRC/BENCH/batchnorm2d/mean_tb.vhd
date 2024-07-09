a62968e0e283e5f05c1025f737890ec2fd835e4c
TimotheeCharrierElsys <timothee.charrier@elsys-design.com>
2024-07-09 16:24:10 +0200

    feat: update mean module architecture

---
+++ SRC/BENCH/batchnorm2d/mean_tb.vhd
@@ -0,0 +1,159 @@
+-----------------------------------------------------------------------------------
+--!     @file    mean_tb
+--!     @brief        This testbench verifies the functionality of the pipelined mac 3*3
+--!     @details      It initializes the inputs, applies test vectors, and checks the outputs.
+--!     @author       Timothée Charrier
+-----------------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
+
+library LIB_RTL;
+use LIB_RTL.types_pkg.all;
+
+entity mean_tb is
+end entity;
+
+architecture mean_tb_arch of mean_tb is
+    -------------------------------------------------------------------------------------
+    -- CONSTANTS
+    -------------------------------------------------------------------------------------
+    constant i_clk_period                     : time    := 10 ns; --! Clock period
+    constant WAIT_COUNT                       : integer := 8;     --! Number clock tics to wait
+    constant BITWIDTH                         : integer := 16;    --! Bit BITWIDTH of each operand
+    constant MATRIX_SIZE                      : integer := 3;     --! Kernel Size
+    constant DIVISION_SCALE_FACTOR_POWER_OF_2 : integer := 10;    --! Scale factor to compute the division by MATRIX_SIZE * MATRIX_SIZE
+    constant CHANNEL_NUMBER                   : integer := 3;
+
+    -------------------------------------------------------------------------------------
+    -- SIGNALS
+    -------------------------------------------------------------------------------------
+    signal clock          : std_logic := '0';                                                                                                 --! Clock signal
+    signal reset_n        : std_logic := '1';                                                                                                 --! Reset signal, active at low state
+    signal i_sys_enable   : std_logic := '0';                                                                                                 --! Enable signal, active at high state
+    signal i_volume       : t_volume(CHANNEL_NUMBER - 1 downto 0)(MATRIX_SIZE - 1 downto 0)(MATRIX_SIZE - 1 downto 0)(BITWIDTH - 1 downto 0); --! Input volume
+    signal i_volume_valid : std_logic := '0';                                                                                                 --! Input volume valid signal
+    signal o_mean         : t_vec(CHANNEL_NUMBER - 1 downto 0)(BITWIDTH - 1 downto 0);                                                        --! Channel-wise output mean
+    signal o_mean_done    : std_logic;                                                                                                        --! Output valid signal
+
+    -------------------------------------------------------------------------------------
+    -- COMPONENTS
+    -------------------------------------------------------------------------------------
+    component mean
+        generic (
+            BITWIDTH                         : integer;
+            MATRIX_SIZE                      : integer;
+            CHANNEL_NUMBER                   : integer;
+            DIVISION_SCALE_FACTOR_POWER_OF_2 : integer
+        );
+        port (
+            clock          : in std_logic;
+            reset_n        : in std_logic;
+            i_sys_enable   : in std_logic;
+            i_volume       : in t_volume(CHANNEL_NUMBER - 1 downto 0)(MATRIX_SIZE - 1 downto 0)(MATRIX_SIZE - 1 downto 0)(BITWIDTH - 1 downto 0);
+            i_volume_valid : in std_logic;
+            o_mean         : out t_vec(CHANNEL_NUMBER - 1 downto 0)(BITWIDTH - 1 downto 0);
+            o_mean_done    : out std_logic
+        );
+    end component;
+
+begin
+    -------------------------------------------------------------------------------------
+    -- UNIT UNDER TEST (UUT)
+    -------------------------------------------------------------------------------------
+    UUT : mean
+    generic map(
+        BITWIDTH                         => BITWIDTH,
+        MATRIX_SIZE                      => MATRIX_SIZE,
+        CHANNEL_NUMBER                   => CHANNEL_NUMBER,
+        DIVISION_SCALE_FACTOR_POWER_OF_2 => DIVISION_SCALE_FACTOR_POWER_OF_2
+    )
+    port map(
+        clock          => clock,
+        reset_n        => reset_n,
+        i_sys_enable   => i_sys_enable,
+        i_volume       => i_volume,
+        i_volume_valid => i_volume_valid,
+        o_mean         => o_mean,
+        o_mean_done    => o_mean_done
+    );
+
+    -- Clock generation
+    clock <= not clock after i_clk_period / 2;
+
+    -------------------------------------------------------------------------------------
+    -- TEST PROCESS
+    -------------------------------------------------------------------------------------
+    stimulus : process
+    begin
+        -- Reset the system
+        reset_n <= '0';
+        wait for 2 * i_clk_period;
+        reset_n <= '1';
+
+        -- Enable the mac unit
+        i_sys_enable <= '1';
+
+        -- Apply input vectors
+        i_volume(0)(0)(0) <= std_logic_vector(to_unsigned(1, BITWIDTH));
+        i_volume(0)(0)(1) <= std_logic_vector(to_unsigned(2, BITWIDTH));
+        i_volume(0)(0)(2) <= std_logic_vector(to_unsigned(3, BITWIDTH));
+        i_volume(0)(1)(0) <= std_logic_vector(to_unsigned(4, BITWIDTH));
+        i_volume(0)(1)(1) <= std_logic_vector(to_unsigned(5, BITWIDTH));
+        i_volume(0)(1)(2) <= std_logic_vector(to_unsigned(6, BITWIDTH));
+        i_volume(0)(2)(0) <= std_logic_vector(to_unsigned(7, BITWIDTH));
+        i_volume(0)(2)(1) <= std_logic_vector(to_unsigned(8, BITWIDTH));
+        i_volume(0)(2)(2) <= std_logic_vector(to_unsigned(9, BITWIDTH));
+
+        i_volume(1)(0)(0) <= std_logic_vector(to_unsigned(10, BITWIDTH));
+        i_volume(1)(0)(1) <= std_logic_vector(to_unsigned(11, BITWIDTH));
+        i_volume(1)(0)(2) <= std_logic_vector(to_unsigned(12, BITWIDTH));
+        i_volume(1)(1)(0) <= std_logic_vector(to_unsigned(13, BITWIDTH));
+        i_volume(1)(1)(1) <= std_logic_vector(to_unsigned(14, BITWIDTH));
+        i_volume(1)(1)(2) <= std_logic_vector(to_unsigned(15, BITWIDTH));
+        i_volume(1)(2)(0) <= std_logic_vector(to_unsigned(16, BITWIDTH));
+        i_volume(1)(2)(1) <= std_logic_vector(to_unsigned(17, BITWIDTH));
+        i_volume(1)(2)(2) <= std_logic_vector(to_unsigned(18, BITWIDTH));
+
+        i_volume(2)(0)(0) <= std_logic_vector(to_unsigned(19, BITWIDTH));
+        i_volume(2)(0)(1) <= std_logic_vector(to_unsigned(20, BITWIDTH));
+        i_volume(2)(0)(2) <= std_logic_vector(to_unsigned(21, BITWIDTH));
+        i_volume(2)(1)(0) <= std_logic_vector(to_unsigned(22, BITWIDTH));
+        i_volume(2)(1)(1) <= std_logic_vector(to_unsigned(23, BITWIDTH));
+        i_volume(2)(1)(2) <= std_logic_vector(to_unsigned(24, BITWIDTH));
+        i_volume(2)(2)(0) <= std_logic_vector(to_unsigned(25, BITWIDTH));
+        i_volume(2)(2)(1) <= std_logic_vector(to_unsigned(26, BITWIDTH));
+        i_volume(2)(2)(2) <= std_logic_vector(to_unsigned(27, BITWIDTH));
+
+        i_volume_valid <= '1';
+        wait for i_clk_period;
+        i_volume_valid <= '0';
+
+        -- Wait for enough time to allow the pipeline to process the inputs
+        wait until o_mean_done = '1';
+
+        -- Check the output
+        assert o_mean(0) = std_logic_vector(to_signed(4, BITWIDTH))
+        report "Test failed: output does not match expected output"
+            severity error;
+        assert o_mean(1) = std_logic_vector(to_signed(13, BITWIDTH))
+        report "Test failed: output does not match expected output"
+            severity error;
+        assert o_mean(2) = std_logic_vector(to_signed(22, BITWIDTH))
+        report "Test failed: output does not match expected output"
+            severity error;
+
+        -- Finish the simulation
+        wait;
+    end process stimulus;
+
+end architecture;
+
+configuration mean_tb_conf of mean_tb is
+    for mean_tb_arch
+        for UUT : mean
+            use configuration LIB_RTL.mean_conf;
+        end for;
+    end for;
+end configuration mean_tb_conf;
---
+++ SRC/RTL/batchnorm2d/mean.vhd
@@ -0,0 +1,308 @@
+-----------------------------------------------------------------------------------
+--!     @file       mean
+--!     @brief      This entity implements a pipelined fully connected layer.
+--!                 It performs multiplication and then additions
+--!     @author     Timothée Charrier
+-----------------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
+use IEEE.MATH_REAL.all;
+
+library LIB_RTL;
+use LIB_RTL.TYPES_PKG.all;
+
+--! Entity mean
+--! This entity implements a full connected layer layer using an adder tree.
+entity mean is
+    generic (
+        BITWIDTH                         : integer := 16; --! Bit width of each operand
+        MATRIX_SIZE                      : integer := 3;  --! Input Maxtrix Size (squared)
+        CHANNEL_NUMBER                   : integer := 3;  --! Number of channels in the input
+        DIVISION_SCALE_FACTOR_POWER_OF_2 : integer := 16  --! Scale factor to compute the division by MATRIX_SIZE * MATRIX_SIZE
+    );
+    port (
+        clock          : in std_logic;                                                                                                        --! Clock signal
+        reset_n        : in std_logic;                                                                                                        --! Reset signal, active low
+        i_sys_enable   : in std_logic;                                                                                                        --! Global enable signal, active high
+        i_volume       : in t_volume(CHANNEL_NUMBER - 1 downto 0)(MATRIX_SIZE - 1 downto 0)(MATRIX_SIZE - 1 downto 0)(BITWIDTH - 1 downto 0); --! Input volume
+        i_volume_valid : in std_logic;                                                                                                        --! Input volume valid signal
+        o_mean         : out t_vec(CHANNEL_NUMBER - 1 downto 0)(BITWIDTH - 1 downto 0);                                                       --! Channel-wise output mean
+        o_mean_done    : out std_logic                                                                                                        --! Output valid signal
+    );
+end mean;
+
+architecture mean_arch of mean is
+
+    -------------------------------------------------------------------------------------
+    -- TYPES
+    -------------------------------------------------------------------------------------
+    type t_computation_signed is array (0 to CHANNEL_NUMBER - 1) of signed(DIVISION_SCALE_FACTOR_POWER_OF_2 + BITWIDTH - 1 downto 0); --! Type to handle the computation easily
+
+    -------------------------------------------------------------------------------------
+    -- CONSTANTS
+    -------------------------------------------------------------------------------------
+    constant DIVISION_SCALE_FACTOR : integer := 2 ** DIVISION_SCALE_FACTOR_POWER_OF_2;
+    constant MEAN_DIVISION_FACTOR  : integer := DIVISION_SCALE_FACTOR / (MATRIX_SIZE * MATRIX_SIZE);
+
+    constant N_ADDITION_REG        : integer := 1;                             --! Number of addition registers in adder_tree.
+    constant N_OUTPUT_REG          : integer := 1;                             --! Number of output registers.
+    constant DFF_DELAY_UNPIPELINED : integer := N_ADDITION_REG + N_OUTPUT_REG; --! Total delay when not pipelined
+
+    -------------------------------------------------------------------------------------
+    -- SIGNALS
+    -------------------------------------------------------------------------------------
+    signal flatten_i_volume : t_mat(CHANNEL_NUMBER - 1 downto 0)(MATRIX_SIZE * MATRIX_SIZE - 1 downto 0)(BITWIDTH - 1 downto 0); --! Flattened i_volume
+    signal r_channel_sums   : t_vec(CHANNEL_NUMBER - 1 downto 0)(BITWIDTH - 1 downto 0);                                         --! Channel-wise sums
+
+    -------------------------------------------------------------------------------------
+    -- COMPONENTS
+    -------------------------------------------------------------------------------------
+    component adder_tree
+        generic (
+            N_OPD    : integer;
+            BITWIDTH : integer
+        );
+        port (
+            clock        : in std_logic;                                    --! Clock signal
+            reset_n      : in std_logic;                                    --! Reset signal, active at low state
+            i_sys_enable : in std_logic;                                    --! Reset signal, active at low state
+            i_data       : in t_vec(0 to N_OPD - 1)(BITWIDTH - 1 downto 0); --! Input data vector
+            o_data       : out std_logic_vector(BITWIDTH - 1 downto 0)      --! Output data
+        );
+    end component;
+
+    component pipeline
+        generic (
+            N_STAGES : integer
+        );
+        port (
+            clock        : in std_logic;
+            reset_n      : in std_logic;
+            i_sys_enable : in std_logic;
+            i_data       : in std_logic;
+            o_data       : out std_logic
+        );
+    end component;
+
+begin
+
+    comb_proc : process (i_volume)
+    begin
+        for c in 0 to CHANNEL_NUMBER - 1 loop
+            for i in 0 to MATRIX_SIZE - 1 loop
+                for j in 0 to MATRIX_SIZE - 1 loop
+                    flatten_i_volume(c)(i * MATRIX_SIZE + j) <= i_volume(c)(i)(j);
+                end loop;
+            end loop;
+        end loop;
+    end process comb_proc;
+
+    -------------------------------------------------------------------------------------
+    -- adder_tree INSTANTIATION
+    -------------------------------------------------------------------------------------
+    -- Instantiate the adder trees
+    gen_adder_trees : for i in 0 to CHANNEL_NUMBER - 1 generate
+        fc_layer_inst : adder_tree
+        generic map(
+            N_OPD    => MATRIX_SIZE * MATRIX_SIZE,
+            BITWIDTH => BITWIDTH
+        )
+        port map(
+            clock        => clock,
+            reset_n      => reset_n,
+            i_sys_enable => i_sys_enable,
+            i_data       => flatten_i_volume(i),
+            o_data       => r_channel_sums(i)
+        );
+    end generate gen_adder_trees;
+
+    -------------------------------------------------------------------------------------
+    -- pipeline INSTANTIATION
+    -------------------------------------------------------------------------------------
+    pipeline_inst : pipeline
+    generic map(
+        N_STAGES => DFF_DELAY_UNPIPELINED
+    )
+    port map(
+        clock        => clock,
+        reset_n      => reset_n,
+        i_sys_enable => i_sys_enable,
+        i_data       => i_volume_valid,
+        o_data       => o_mean_done
+    );
+
+    process (clock, reset_n)
+        variable mean_division : t_computation_signed; --! Variable to store the scaled division
+    begin
+        if reset_n = '0' then
+            o_mean <= (others => (others => '0'));
+
+        elsif rising_edge(clock) then
+            if i_sys_enable = '1' then
+
+                for i in 0 to CHANNEL_NUMBER - 1 loop
+
+                    -- Compute the division by N
+                    mean_division(i) := signed(r_channel_sums(i)) * to_signed(MEAN_DIVISION_FACTOR, DIVISION_SCALE_FACTOR_POWER_OF_2);
+                    mean_division(i) := SHIFT_RIGHT(mean_division(i), DIVISION_SCALE_FACTOR_POWER_OF_2);
+                    -- mean_division(i) := mean_division(i) + 1; --! Maybe need due to computation approximations
+
+                    o_mean(i) <= std_logic_vector(resize(mean_division(i), BITWIDTH));
+                end loop;
+            end if;
+        end if;
+    end process;
+end mean_arch;
+
+configuration mean_conf of mean is
+    for mean_arch
+        for gen_adder_trees
+            for all : adder_tree
+                use entity LIB_RTL.adder_tree(adder_tree_arch);
+            end for;
+        end for;
+
+        for all : adder_tree
+            use entity LIB_RTL.adder_tree(adder_tree_arch);
+        end for;
+    end for;
+end configuration mean_conf;
+
+architecture mean_pipelined_arch of mean is
+
+    -------------------------------------------------------------------------------------
+    -- TYPES
+    -------------------------------------------------------------------------------------
+    type t_computation_signed is array (0 to CHANNEL_NUMBER - 1) of signed(DIVISION_SCALE_FACTOR_POWER_OF_2 + BITWIDTH - 1 downto 0); --! Type to handle the computation easily
+
+    -------------------------------------------------------------------------------------
+    -- CONSTANTS
+    -------------------------------------------------------------------------------------
+    constant DIVISION_SCALE_FACTOR : integer := 2 ** DIVISION_SCALE_FACTOR_POWER_OF_2;
+    constant MEAN_DIVISION_FACTOR  : integer := DIVISION_SCALE_FACTOR / (MATRIX_SIZE * MATRIX_SIZE);
+
+    constant N_STAGES            : integer := integer(ceil(log2(real(MATRIX_SIZE * MATRIX_SIZE)))); --! Number of stages required to complete the addition process.
+    constant N_ADDITION_REG      : integer := 1;                                                    --! Number of addition registers.
+    constant N_OUTPUT_REG        : integer := 1;                                                    --! Number of output registers.
+    constant DFF_DELAY_PIPELINED : integer := N_STAGES + N_ADDITION_REG + N_OUTPUT_REG;             --! Total delay due to flip-flops when pipelined.
+
+    -------------------------------------------------------------------------------------
+    -- SIGNALS
+    -------------------------------------------------------------------------------------
+    signal flatten_i_volume : t_mat(CHANNEL_NUMBER - 1 downto 0)(MATRIX_SIZE * MATRIX_SIZE - 1 downto 0)(BITWIDTH - 1 downto 0); --! Flattened i_volume
+    signal r_channel_sums   : t_vec(CHANNEL_NUMBER - 1 downto 0)(BITWIDTH - 1 downto 0);                                         --! Channel-wise sums
+
+    -------------------------------------------------------------------------------------
+    -- COMPONENTS
+    -------------------------------------------------------------------------------------
+    component adder_tree
+        generic (
+            N_OPD    : integer;
+            BITWIDTH : integer
+        );
+        port (
+            clock        : in std_logic;                                    --! Clock signal
+            reset_n      : in std_logic;                                    --! Reset signal, active at low state
+            i_sys_enable : in std_logic;                                    --! Reset signal, active at low state
+            i_data       : in t_vec(0 to N_OPD - 1)(BITWIDTH - 1 downto 0); --! Input data vector
+            o_data       : out std_logic_vector(BITWIDTH - 1 downto 0)      --! Output data
+        );
+    end component;
+
+    component pipeline
+        generic (
+            N_STAGES : integer
+        );
+        port (
+            clock        : in std_logic;
+            reset_n      : in std_logic;
+            i_sys_enable : in std_logic;
+            i_data       : in std_logic;
+            o_data       : out std_logic
+        );
+    end component;
+
+begin
+
+    comb_proc : process (i_volume)
+    begin
+        for c in 0 to CHANNEL_NUMBER - 1 loop
+            for i in 0 to MATRIX_SIZE - 1 loop
+                for j in 0 to MATRIX_SIZE - 1 loop
+                    flatten_i_volume(c)(i * MATRIX_SIZE + j) <= i_volume(c)(i)(j);
+                end loop;
+            end loop;
+        end loop;
+    end process comb_proc;
+
+    -------------------------------------------------------------------------------------
+    -- adder_tree INSTANTIATION
+    -------------------------------------------------------------------------------------
+    -- Instantiate the adder trees
+    gen_adder_trees : for i in 0 to CHANNEL_NUMBER - 1 generate
+        fc_layer_inst : adder_tree
+        generic map(
+            N_OPD    => MATRIX_SIZE * MATRIX_SIZE,
+            BITWIDTH => BITWIDTH
+        )
+        port map(
+            clock        => clock,
+            reset_n      => reset_n,
+            i_sys_enable => i_sys_enable,
+            i_data       => flatten_i_volume(i),
+            o_data       => r_channel_sums(i)
+        );
+    end generate gen_adder_trees;
+
+    -------------------------------------------------------------------------------------
+    -- pipeline INSTANTIATION
+    -------------------------------------------------------------------------------------
+    pipeline_inst : pipeline
+    generic map(
+        N_STAGES => DFF_DELAY_PIPELINED
+    )
+    port map(
+        clock        => clock,
+        reset_n      => reset_n,
+        i_sys_enable => i_sys_enable,
+        i_data       => i_volume_valid,
+        o_data       => o_mean_done
+    );
+
+    process (clock, reset_n)
+        variable mean_division : t_computation_signed; --! Variable to store the scaled division
+    begin
+        if reset_n = '0' then
+            o_mean <= (others => (others => '0'));
+
+        elsif rising_edge(clock) then
+            if i_sys_enable = '1' then
+
+                for i in 0 to CHANNEL_NUMBER - 1 loop
+
+                    -- Compute the division by N
+                    mean_division(i) := signed(r_channel_sums(i)) * to_signed(MEAN_DIVISION_FACTOR, DIVISION_SCALE_FACTOR_POWER_OF_2);
+                    mean_division(i) := SHIFT_RIGHT(mean_division(i), DIVISION_SCALE_FACTOR_POWER_OF_2);
+                    -- mean_division(i) := mean_division(i) + 1; --! Maybe need due to computation approximations
+
+                    o_mean(i) <= std_logic_vector(resize(mean_division(i), BITWIDTH));
+                end loop;
+            end if;
+        end if;
+    end process;
+end mean_pipelined_arch;
+
+configuration mean_pipelined_conf of mean is
+    for mean_pipelined_arch
+        for gen_adder_trees
+            for all : adder_tree
+                use entity LIB_RTL.adder_tree(adder_tree_pipelined_arch);
+            end for;
+        end for;
+        for all : adder_tree
+            use entity LIB_RTL.adder_tree(adder_tree_pipelined_arch);
+        end for;
+    end for;
+end configuration mean_pipelined_conf;
