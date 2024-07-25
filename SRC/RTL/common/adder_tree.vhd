579e49737a148aa2d0dc0dfb0d2b3b8177a62a84
timothee.charrier@elsys-design.com <timothee.charrier@elsys-design.com>
2024-07-25 09:46:31 +0200

    feat: improved and update code for adder tree

--- SRC/RTL/common/adder_tree.vhd
+++ SRC/RTL/common/adder_tree.vhd
@@ -30,132 +30,94 @@ entity adder_tree is
     );
 end adder_tree;
 
-architecture adder_tree_pipelined_arch of adder_tree is
-
-    -------------------------------------------------------------------------------------
-    -- CONSTANTS
-    -------------------------------------------------------------------------------------
-    constant N_STAGES : integer := integer(ceil(log2(real(N_OPD)))); --! Number of stages required to complete the addition process.
-
-    -------------------------------------------------------------------------------------
-    -- SIGNALS
-    -------------------------------------------------------------------------------------
-    signal r_next : t_mat(0 to N_STAGES)(0 to (2 ** N_STAGES) - 1)(BITWIDTH - 1 downto 0); --! Next state of the pipeline registers
-    signal r_reg  : t_mat(0 to N_STAGES)(0 to (2 ** N_STAGES) - 1)(BITWIDTH - 1 downto 0); --! Current state of the pipeline registers
-
-begin
-
-    -------------------------------------------------------------------------------------
-    -- INPUT GENERATION
-    -------------------------------------------------------------------------------------
-    -- This generate block assigns the input data to the appropriate stage of the adder tree.
-    input_gen : for i in 0 to N_OPD - 1 generate
-        r_next(N_STAGES)(i) <= i_data(i);
-    end generate;
-
-    -------------------------------------------------------------------------------------
-    -- PADDING GENERATION
-    -------------------------------------------------------------------------------------
-    -- This generate block pads the remaining positions with zeros if the number of operands is less than 2^N_STAGES.
-    pading_gen : if N_OPD < 2 ** N_STAGES generate
-        r_next(N_STAGES)(N_OPD to (2 ** N_STAGES) - 1) <= (others => (others => '0'));
-    end generate;
-
-    -------------------------------------------------------------------------------------
-    -- STAGE GENERATION
-    -------------------------------------------------------------------------------------
-    -- This generate block creates the tree structure of the adder, reducing the number of operands by half in each stage.
-    stage_gen : for i in (N_STAGES - 1) downto 0 generate
-        row_gen : for j in 0 to (2 ** i - 1) generate
-            r_next(i)(j) <= std_logic_vector(signed(r_reg(i + 1)(2 * j)) + signed(r_reg(i + 1)(2 * j + 1)));
-        end generate;
-    end generate;
-
-    -------------------------------------------------------------------------------------
-    -- PROCESS ASYNC (reset negative)
-    -------------------------------------------------------------------------------------
-    --! Process
-    --! Handles the synchronous and asynchronous operations of the pipelined adder.
-    pipeline_control : process (clock, reset_n)
-    begin
-        if reset_n = '0' then
-            -- Initialize the register with zeros on reset
-            r_reg <= (others => (others => (others => '0')));
-
-        elsif rising_edge(clock) then
-            if i_sys_enable = '1' then
-                r_reg <= r_next; -- Transfer next state to current state on rising edge of the clock
-            end if;
-        end if;
-    end process pipeline_control;
-
-    -- Output update
-    o_data <= r_reg(0)(0);
-
-end adder_tree_pipelined_arch;
-
 architecture adder_tree_arch of adder_tree is
 
-    -------------------------------------------------------------------------------------
-    -- CONSTANTS
-    -------------------------------------------------------------------------------------
-    constant N_STAGES : integer := integer(ceil(log2(real(N_OPD)))); --! Number of stages required to complete the addition process.
+    signal reg_i_data  : t_vec(N_OPD - 1 downto 0)(BITWIDTH - 1 downto 0);
+    signal data_vector : t_vec(0 to (N_OPD - 1))(BITWIDTH - 1 downto 0);
 
-    -------------------------------------------------------------------------------------
-    -- SIGNALS
-    -------------------------------------------------------------------------------------
-    signal intermediate_sums : t_mat(0 to N_STAGES)(0 to (2 ** N_STAGES) - 1)(BITWIDTH - 1 downto 0);
-    signal r_output          : std_logic_vector(BITWIDTH - 1 downto 0);
+    signal sum     : std_logic_vector((BITWIDTH - 1) downto 0);
+    signal reg_sum : std_logic_vector((BITWIDTH - 1) downto 0);
 
 begin
 
-    -------------------------------------------------------------------------------------
-    -- INPUT GENERATION
-    -------------------------------------------------------------------------------------
-    -- This generate block assigns the input data to the appropriate stage of the adder tree.
-    input_gen : for i in 0 to N_OPD - 1 generate
-        intermediate_sums(N_STAGES)(i) <= i_data(i);
-    end generate;
-
-    -------------------------------------------------------------------------------------
-    -- PADDING GENERATION
-    -------------------------------------------------------------------------------------
-    -- This generate block pads the remaining positions with zeros if the number of operands is less than 2^N_STAGES.
-    pading_gen : if N_OPD < 2 ** N_STAGES generate
-        intermediate_sums(N_STAGES)(N_OPD to (2 ** N_STAGES) - 1) <= (others => (others => '0'));
-    end generate;
-
-    -------------------------------------------------------------------------------------
-    -- STAGE GENERATION
-    -------------------------------------------------------------------------------------
-    -- This generate block creates the tree structure of the adder, reducing the number of operands by half in each stage.
-    stage_gen : for i in (N_STAGES - 1) downto 0 generate
-        row_gen : for j in 0 to (2 ** i - 1) generate
-            intermediate_sums(i)(j) <= std_logic_vector(
-            signed(intermediate_sums(i + 1)(2 * j)) + signed(intermediate_sums(i + 1)(2 * j + 1))
-            );
-        end generate;
-    end generate;
-
-    -------------------------------------------------------------------------------------
-    -- PROCESS ASYNC (reset negative)
-    -------------------------------------------------------------------------------------
-    --! Process
-    --! Handles the synchronous and asynchronous operations of the pipelined adder.
-    pipeline_control : process (clock, reset_n)
+    process (clock, reset_n)
     begin
         if reset_n = '0' then
-            -- Initialize the register with zeros on reset
-            r_output <= (others => '0');
 
         elsif rising_edge(clock) then
-            if i_sys_enable = '1' then
-                r_output <= intermediate_sums(0)(0);
+            if (i_sys_enable = '1') then
+                for TERM_INDEX in 0 to (N_OPD - 1) loop
+                    reg_i_data(TERM_INDEX) <= i_data(TERM_INDEX);
+                end loop;
+                reg_sum <= sum;
             end if;
         end if;
-    end process pipeline_control;
+    end process;
 
-    -- Output update
-    o_data <= r_output;
+    gen_data_vector : process (reg_i_data)
+    begin
+        for TERM_INDEX in 0 to (N_OPD - 1) loop
+            data_vector(TERM_INDEX) <= reg_i_data(TERM_INDEX);
+        end loop;
+    end process gen_data_vector;
 
-end adder_tree_arch;
+    gen_sum : process (data_vector)
+        variable accumulator : std_logic_vector((BITWIDTH - 1) downto 0);
+    begin
+        accumulator := std_logic_vector(to_signed(0, accumulator'length));
+        for TERM_INDEX in 0 to (N_OPD - 1) loop
+            accumulator := std_logic_vector(signed(accumulator) + signed(data_vector(TERM_INDEX)));
+        end loop;
+        sum <= accumulator;
+    end process gen_sum;
+
+    o_data <= reg_sum;
+
+end architecture adder_tree_arch;
+
+architecture adder_tree_pipelined_arch of adder_tree is
+
+    signal reg_i_data       : t_vec(N_OPD - 1 downto 0)(BITWIDTH - 1 downto 0);
+    signal data_vector      : t_vec(0 to (N_OPD - 1))(BITWIDTH - 1 downto 0);
+    signal data_vector_next : t_vec(0 to (N_OPD - 1))(BITWIDTH - 1 downto 0);
+
+    signal sum     : std_logic_vector((BITWIDTH - 1) downto 0);
+    signal reg_sum : std_logic_vector((BITWIDTH - 1) downto 0);
+
+begin
+
+    process (clock, reset_n)
+    begin
+        if reset_n = '0' then
+
+        elsif rising_edge(clock) then
+            if (i_sys_enable = '1') then
+                for TERM_INDEX in 0 to (N_OPD - 1) loop
+                    reg_i_data(TERM_INDEX) <= i_data(TERM_INDEX);
+                end loop;
+                reg_sum          <= sum;
+                data_vector_next <= data_vector;
+            end if;
+        end if;
+    end process;
+
+    gen_data_vector : process (reg_i_data)
+    begin
+        for TERM_INDEX in 0 to (N_OPD - 1) loop
+            data_vector(TERM_INDEX) <= reg_i_data(TERM_INDEX);
+        end loop;
+    end process gen_data_vector;
+
+    gen_sum : process (data_vector_next)
+        variable accumulator : std_logic_vector((BITWIDTH - 1) downto 0);
+    begin
+        accumulator := std_logic_vector(to_signed(0, accumulator'length));
+        for TERM_INDEX in 0 to (N_OPD - 1) loop
+            accumulator := std_logic_vector(signed(accumulator) + signed(data_vector_next(TERM_INDEX)));
+        end loop;
+        sum <= accumulator;
+    end process gen_sum;
+
+    o_data <= reg_sum;
+
+end architecture adder_tree_pipelined_arch;
