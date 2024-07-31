ddb6fe7b7fa8bfdd2d04c6aafa7e593b940cefad
timothee.charrier@elsys-design.com <timothee.charrier@elsys-design.com>
2024-07-31 16:57:46 +0200

    feat: add new architecture

--- SRC/BENCH/mac/accumulative_mac_tb.vhd
+++ SRC/BENCH/mac/accumulative_mac_tb.vhd
@@ -19,36 +19,42 @@ architecture accumulative_mac_tb_arch of accumulative_mac_tb is
     -------------------------------------------------------------------------------------
     -- CONSTANTS
     -------------------------------------------------------------------------------------
-    constant i_clk_period : time    := 10 ns; --! Clock period
-    constant WAIT_COUNT   : integer := 8;     --! Number clock tics to wait
-    constant BITWIDTH     : integer := 8;     --! Bit BITWIDTH of each operand
+    constant i_clk_period      : time      := 10 ns; --! Clock period
+    constant WAIT_COUNT        : integer   := 8;     --! Number clock tics to wait
+    constant DO_MULTIPLICATION : std_logic := '0';   --! 
+    constant INTPUT_WIDTH      : integer   := 8;     --! Bit width of input operands
+    constant OUTPUT_WIDTH      : integer   := 16;    --! Bit width of output result
 
     -------------------------------------------------------------------------------------
     -- SIGNALS
     -------------------------------------------------------------------------------------
-    signal clock         : std_logic := '0';                            --! Clock signal
-    signal reset_n       : std_logic := '1';                            --! Reset signal, active at low state
-    signal i_sys_enable  : std_logic := '0';                            --! Enable signal, active at high state
-    signal i_clear       : std_logic := '0';                            --! Clear signal, active high
-    signal i_multiplier1 : std_logic_vector(BITWIDTH - 1 downto 0);     --! First mult operand
-    signal i_multiplier2 : std_logic_vector(BITWIDTH - 1 downto 0);     --! Second mult operand
-    signal o_result      : std_logic_vector(2 * BITWIDTH - 1 downto 0); --! Output data
+    signal clock        : std_logic := '0';                            --! Clock signal
+    signal reset_n      : std_logic := '1';                            --! Reset signal, active at low state
+    signal i_sys_enable : std_logic := '0';                            --! Enable signal, active at high state
+    signal i_clear      : std_logic := '0';                            --! Clear signal, active high
+    signal i_enable     : std_logic := '0';                            --! Input enable, active high
+    signal i_operand1   : std_logic_vector(INTPUT_WIDTH - 1 downto 0); --! First mult operand
+    signal i_operand2   : std_logic_vector(INTPUT_WIDTH - 1 downto 0); --! Second mult operand
+    signal o_result     : std_logic_vector(OUTPUT_WIDTH - 1 downto 0); --! Output data
 
     -------------------------------------------------------------------------------------
     -- COMPONENTS
     -------------------------------------------------------------------------------------
     component accumulative_mac
         generic (
-            BITWIDTH : integer
+            DO_MULTIPLICATION : std_logic;
+            INTPUT_WIDTH      : integer;
+            OUTPUT_WIDTH      : integer
         );
         port (
-            clock         : in std_logic;
-            reset_n       : in std_logic;
-            i_sys_enable  : in std_logic;
-            i_clear       : in std_logic;
-            i_multiplier1 : in std_logic_vector(BITWIDTH - 1 downto 0);
-            i_multiplier2 : in std_logic_vector(BITWIDTH - 1 downto 0);
-            o_result      : out std_logic_vector(2 * BITWIDTH - 1 downto 0)
+            clock        : in std_logic;
+            reset_n      : in std_logic;
+            i_sys_enable : in std_logic;
+            i_clear      : in std_logic;
+            i_enable     : in std_logic;
+            i_operand1   : in std_logic_vector(INTPUT_WIDTH - 1 downto 0);
+            i_operand2   : in std_logic_vector(INTPUT_WIDTH - 1 downto 0);
+            o_result     : out std_logic_vector(OUTPUT_WIDTH - 1 downto 0)
         );
     end component;
 
@@ -58,16 +64,19 @@ begin
     -------------------------------------------------------------------------------------
     UUT : accumulative_mac
     generic map(
-        BITWIDTH => BITWIDTH
+        DO_MULTIPLICATION => DO_MULTIPLICATION,
+        INTPUT_WIDTH      => INTPUT_WIDTH,
+        OUTPUT_WIDTH      => OUTPUT_WIDTH
     )
     port map(
-        clock         => clock,
-        reset_n       => reset_n,
-        i_sys_enable  => i_sys_enable,
-        i_clear       => i_clear,
-        i_multiplier1 => i_multiplier1,
-        i_multiplier2 => i_multiplier2,
-        o_result      => o_result
+        clock        => clock,
+        reset_n      => reset_n,
+        i_sys_enable => i_sys_enable,
+        i_clear      => i_clear,
+        i_enable     => i_enable,
+        i_operand1   => i_operand1,
+        i_operand2   => i_operand2,
+        o_result     => o_result
     );
     -- Clock generation
     clock <= not clock after i_clk_period / 2;
@@ -79,32 +88,37 @@ begin
     begin
         -- Reset the system
         reset_n <= '0';
-        wait for 2 * i_clk_period;
+        wait for 15 ns;
         reset_n <= '1';
 
         -- Enable the mac unit
         i_sys_enable <= '1';
+        i_enable     <= '1';
 
         -- Apply input vectors
-        i_multiplier1 <= std_logic_vector(to_signed(5, BITWIDTH));
-        i_multiplier2 <= std_logic_vector(to_signed(7, BITWIDTH));
+        i_operand1 <= std_logic_vector(to_signed(5, INTPUT_WIDTH));
+        i_operand2 <= std_logic_vector(to_signed(7, INTPUT_WIDTH));
 
-        -- Wait for enough time to allow the pipeline to process the inputs
-        wait for (WAIT_COUNT * i_clk_period);
+        wait for i_clk_period + 1 ns;
 
         -- Check the output
-        assert o_result = std_logic_vector(to_signed(45, 2 * BITWIDTH))
+        assert o_result = std_logic_vector(to_signed(35, OUTPUT_WIDTH))
         report "Test failed: output does not match expected output"
             severity error;
+
+        wait for i_clk_period;
 
         i_clear <= '1';
         wait for i_clk_period;
         i_clear <= '0';
-        wait for i_clk_period;
+        wait for i_clk_period/2;
 
-        assert o_result = std_logic_vector(to_signed(0, 2 * BITWIDTH))
+        assert o_result = std_logic_vector(to_signed(0, OUTPUT_WIDTH))
         report "Test failed: output not cleared correctly"
             severity error;
+
+        wait for i_clk_period;
+        i_enable <= '0';
 
         -- Finish the simulation
         wait;
--- SRC/RTL/mac/accumulative_mac.vhd
+++ SRC/RTL/mac/accumulative_mac.vhd
@@ -15,16 +15,18 @@ use IEEE.NUMERIC_STD.all;
 --! It multiplies two operands and then adds the output.
 entity accumulative_mac is
     generic (
-        BITWIDTH : integer := 8 --! Bit width of each operand
+        DO_MULTIPLICATION : std_logic := '1'; --! Define if it is a mac ('1') or an additionner ('0')
+        INTPUT_WIDTH      : integer   := 8;   --! Bit width of input operands
+        OUTPUT_WIDTH      : integer   := 16   --! Bit width of output result
     );
     port (
-        clock         : in std_logic;                                   --! Clock signal
-        reset_n       : in std_logic;                                   --! Reset signal, active low
-        i_sys_enable  : in std_logic;                                   --! Global enable signal, active high
-        i_clear       : in std_logic;                                   --! Clear signal, active high
-        i_multiplier1 : in std_logic_vector(BITWIDTH - 1 downto 0);     --! First multiplication operand
-        i_multiplier2 : in std_logic_vector(BITWIDTH - 1 downto 0);     --! Second multiplication operand
-        o_result      : out std_logic_vector(2 * BITWIDTH - 1 downto 0) --! Output result value
+        clock        : in std_logic;                                   --! Clock signal
+        reset_n      : in std_logic;                                   --! Reset signal, active low
+        i_sys_enable : in std_logic;                                   --! Global enable signal, active high
+        i_clear      : in std_logic;                                   --! Clear signal, active high
+        i_operand1   : in std_logic_vector(INTPUT_WIDTH - 1 downto 0); --! First multiplication operand
+        i_operand2   : in std_logic_vector(INTPUT_WIDTH - 1 downto 0); --! Second multiplication operand
+        o_result     : out std_logic_vector(OUTPUT_WIDTH - 1 downto 0) --! Output result value
     );
 end accumulative_mac;
 
@@ -33,9 +35,26 @@ architecture accumulative_mac_arch of accumulative_mac is
     -------------------------------------------------------------------------------------
     -- SIGNALS
     -------------------------------------------------------------------------------------
-    signal mac_out : std_logic_vector(2 * BITWIDTH - 1 downto 0); --! MUX output value
+    signal mac_out               : std_logic_vector(OUTPUT_WIDTH - 1 downto 0); --! MUX output value
+    signal multiplication_result : std_logic_vector(OUTPUT_WIDTH - 1 downto 0);
+    signal sum_result            : std_logic_vector(OUTPUT_WIDTH downto 0);
 
 begin
+
+    gen_multiplication : if DO_MULTIPLICATION = '1' generate
+        process (all)
+        begin
+            multiplication_result <= std_logic_vector(signed(i_operand1) * signed(i_operand2));
+            sum_result            <= std_logic_vector(resize(signed(mac_out), OUTPUT_WIDTH + 1) + resize(signed(multiplication_result), OUTPUT_WIDTH + 1));
+        end process;
+    end generate gen_multiplication;
+
+    do_not_gen_multiplication : if DO_MULTIPLICATION = '0' generate
+        process (all)
+        begin
+            sum_result <= std_logic_vector(resize(signed(i_operand1), OUTPUT_WIDTH + 1) + resize(signed(mac_out), OUTPUT_WIDTH + 1));
+        end process;
+    end generate do_not_gen_multiplication;
 
     -------------------------------------------------------------------------------------
     -- PROCESS ASYNC (reset negative)
@@ -43,8 +62,6 @@ begin
     --! Process
     --! Handles the synchronous and asynchronous operations of the MAC unit.
     process (clock, reset_n)
-        variable mult_result : std_logic_vector(2 * BITWIDTH - 1 downto 0); --! Multiplication result
-        variable sum_result  : std_logic_vector(2 * BITWIDTH downto 0);     --! Sum result with extra bit for overflow detection
     begin
         if reset_n = '0' then
             -- Reset output register to zeros
@@ -54,19 +71,7 @@ begin
                 if i_clear = '1' then
                     mac_out <= (others => '0');
                 else
-                    -- Multiplication
-                    mult_result := std_logic_vector(signed(i_multiplier1) * signed(i_multiplier2));
-
-                    -- Addition with overflow detection
-                    sum_result := std_logic_vector(resize(signed(mac_out), 2 * BITWIDTH + 1) + resize(signed(mult_result), 2 * BITWIDTH + 1));
-
-                    -- Check for overflow and handle it
-                    if sum_result(2 * BITWIDTH) /= sum_result(2 * BITWIDTH - 1) then
-                        -- Overflow occurred, handle accordingly (e.g., saturate or wrap around)
-                        mac_out <= (others => '1');
-                    else
-                        mac_out <= sum_result(2 * BITWIDTH - 1 downto 0);
-                    end if;
+                    mac_out <= sum_result(OUTPUT_WIDTH - 1 downto 0);
                 end if;
             end if;
         end if;
