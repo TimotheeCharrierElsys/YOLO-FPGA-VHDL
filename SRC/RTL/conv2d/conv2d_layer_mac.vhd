4d56598880beb22ee8b823cbea52746e88d82c9e
timothee.charrier@elsys-design.com <timothee.charrier@elsys-design.com>
2024-07-31 16:59:33 +0200

    chore: first draft of new design (not working)

--- SRC/RTL/conv2d/conv2d_layer_mac.vhd
+++ SRC/RTL/conv2d/conv2d_layer_mac.vhd
@@ -43,42 +43,34 @@ architecture conv2d_layer_mac_arch of conv2d_layer_mac is
     -------------------------------------------------------------------------------------
     -- SIGNALS
     -------------------------------------------------------------------------------------
-    signal r_results                : t_vec(CHANNEL_NUMBER downto 0)(2 * BITWIDTH - 1 downto 0); --! Intermediate signal to hold the output of each MAC unit for each channel.
-    signal current_row              : integer range 0 to KERNEL_SIZE - 1;                        --! Counter to track the current position within the kernel.
-    signal current_col              : integer range 0 to KERNEL_SIZE - 1;                        --! Counter to track the current position within the kernel.
-    signal intermediate_multiplier1 : t_vec(CHANNEL_NUMBER - 1 downto 0)(BITWIDTH - 1 downto 0); --! Intermediate signal to avoid static 
-    signal intermediate_multiplier2 : t_vec(CHANNEL_NUMBER - 1 downto 0)(BITWIDTH - 1 downto 0); --! Intermediate signal
+    signal r_results                 : t_vec(CHANNEL_NUMBER downto 0)(2 * BITWIDTH - 1 downto 0); --! Intermediate signal to hold the output of each MAC unit for each channel.
+    signal current_row               : integer range 0 to KERNEL_SIZE - 1;                        --! Counter to track the current position within the kernel.
+    signal current_col               : integer range 0 to KERNEL_SIZE - 1;                        --! Counter to track the current position within the kernel.
+    signal current_channel           : integer range 0 to CHANNEL_NUMBER;                         --! Counter to track the current position within the channels.
+    signal intermediate_multiplier1  : t_vec(CHANNEL_NUMBER - 1 downto 0)(BITWIDTH - 1 downto 0); --! Intermediate signal to avoid static 
+    signal intermediate_multiplier2  : t_vec(CHANNEL_NUMBER - 1 downto 0)(BITWIDTH - 1 downto 0); --! Intermediate signal
+    signal current_operand           : std_logic_vector(2 * BITWIDTH - 1 downto 0);               --! Intermediate signal for output sum
+    signal clear_output_sum          : std_logic;
+    signal start_processing          : std_logic; --! Signal to start processing
+    signal data_valid_previous_state : std_logic; --! Previous state of the data_valid signal
 
     -------------------------------------------------------------------------------------
     -- COMPONENTS
     -------------------------------------------------------------------------------------
     component accumulative_mac
         generic (
-            BITWIDTH : integer
-        );
-        port (
-            clock         : in std_logic;
-            reset_n       : in std_logic;
-            i_sys_enable  : in std_logic;
-            i_clear       : in std_logic;
-            i_multiplier1 : in std_logic_vector(BITWIDTH - 1 downto 0);
-            i_multiplier2 : in std_logic_vector(BITWIDTH - 1 downto 0);
-            o_result      : out std_logic_vector(2 * BITWIDTH - 1 downto 0)
-        );
-    end component;
-
-    component adder_tree
-        generic (
-            DO_PIPELINE  : std_logic;
-            NUM_OPERANDS : integer;
-            BITWIDTH     : integer
+            DO_MULTIPLICATION : std_logic;
+            INTPUT_WIDTH      : integer;
+            OUTPUT_WIDTH      : integer
         );
         port (
             clock        : in std_logic;
             reset_n      : in std_logic;
             i_sys_enable : in std_logic;
-            i_operands   : in t_vec(NUM_OPERANDS - 1 downto 0)(BITWIDTH - 1 downto 0);
-            o_result     : out std_logic_vector(BITWIDTH - 1 downto 0)
+            i_clear      : in std_logic;
+            i_operand1   : in std_logic_vector(INTPUT_WIDTH - 1 downto 0);
+            i_operand2   : in std_logic_vector(INTPUT_WIDTH - 1 downto 0);
+            o_result     : out std_logic_vector(OUTPUT_WIDTH - 1 downto 0)
         );
     end component;
 
@@ -92,31 +84,34 @@ begin
         --! Instantiate one accumualtive mac for each channel
         gen_accumulative_mac_inst : accumulative_mac
         generic map(
-            BITWIDTH => BITWIDTH
+            DO_MULTIPLICATION => '1',
+            INTPUT_WIDTH      => BITWIDTH,
+            OUTPUT_WIDTH      => 2 * BITWIDTH
         )
         port map(
-            clock         => clock,
-            reset_n       => reset_n,
-            i_sys_enable  => i_sys_enable,
-            i_clear       => i_valid,
-            i_multiplier1 => intermediate_multiplier1(i),
-            i_multiplier2 => intermediate_multiplier2(i),
-            o_result      => r_results(i)
+            clock        => clock,
+            reset_n      => reset_n,
+            i_sys_enable => i_sys_enable,
+            i_clear      => i_valid,
+            i_operand1   => intermediate_multiplier1(i),
+            i_operand2   => intermediate_multiplier2(i),
+            o_result     => r_results(i)
         );
-
     end generate gen_mac_channel;
 
-    adder_tree_inst : adder_tree
+    gen_output_sum : accumulative_mac
     generic map(
-        DO_PIPELINE  => DO_PIPELINE,
-        NUM_OPERANDS => CHANNEL_NUMBER + 1,
-        BITWIDTH     => 2 * BITWIDTH
+        DO_MULTIPLICATION => '0',
+        INTPUT_WIDTH      => 2 * BITWIDTH,
+        OUTPUT_WIDTH      => 2 * BITWIDTH
     )
     port map(
         clock        => clock,
         reset_n      => reset_n,
         i_sys_enable => i_sys_enable,
-        i_operands   => r_results,
+        i_clear      => clear_output_sum,
+        i_operand1   => current_operand,
+        i_operand2 => (others => '1'),
         o_result     => o_result
     );
 
@@ -129,6 +124,10 @@ begin
         for i in 0 to CHANNEL_NUMBER - 1 loop
             intermediate_multiplier1(i) <= i_data(i)(current_col)(current_row);
             intermediate_multiplier2(i) <= i_kernels(i)(current_col)(current_row);
+            current_operand             <= r_results(current_channel) when current_row /= CHANNEL_NUMBER else
+                i_bias;
+            clear_output_sum <= '1' when current_row = KERNEL_SIZE - 1 and current_col = KERNEL_SIZE - 2 else
+                '0';
         end loop;
     end process;
 
@@ -139,25 +138,45 @@ begin
     begin
         if reset_n = '0' then
             -- Reset counters  to initial states.
-            current_row <= 0;
-            current_col <= 0;
+            data_valid_previous_state <= '0';
+            start_processing          <= '0';
+            current_row               <= 0;
+            current_col               <= 0;
+            current_channel           <= 0;
 
         elsif rising_edge(clock) then
             if i_sys_enable = '1' then
 
-                -- Update counter signals.
-                if current_col = KERNEL_SIZE - 1 then
-                    current_col <= 0;
-                    if current_row = KERNEL_SIZE - 1 then
-                        current_row <= 0;
+                -- Update last data_valid signal
+                data_valid_previous_state <= i_valid;
+
+                -- Check if input data is valid
+                if (start_processing = '0' and i_valid = '1' and data_valid_previous_state = '0') then
+                    start_processing <= '1';
+                end if;
+
+                if start_processing = '1' then
+
+                    -- Update counter signals.
+                    if current_col = KERNEL_SIZE - 1 then
+                        if current_row = KERNEL_SIZE - 1 then
+                            if current_channel = CHANNEL_NUMBER then
+                                start_processing <= '0';
+                                current_channel  <= 0;
+                                current_row      <= 0;
+                                current_col      <= 0;
+                            else
+                                current_channel <= current_channel + 1;
+                            end if;
+                        else
+                            current_row <= current_row + 1;
+                            current_col <= 0;
+                        end if;
                     else
-                        current_row <= current_row + 1;
+                        current_col <= current_col + 1;
                     end if;
-                else
-                    current_col <= current_col + 1;
                 end if;
             end if;
-
         end if;
     end process counter_control;
 end conv2d_layer_mac_arch;
@@ -168,10 +187,6 @@ configuration conv2d_layer_mac_conf of conv2d_layer_mac is
             for all : accumulative_mac
                 use entity LIB_RTL.accumulative_mac(accumulative_mac_arch);
             end for;
-        end for;
-
-        for all : adder_tree
-            use entity LIB_RTL.adder_tree(adder_tree_arch);
         end for;
     end for;
 end configuration conv2d_layer_mac_conf;
