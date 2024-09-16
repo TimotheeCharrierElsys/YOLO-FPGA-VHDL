c7684249e86a0184f7b99b2f9dcd8f9e3fa41687
timothee.charrier@elsys-design.com <timothee.charrier@elsys-design.com>
2024-09-16 17:00:12 +0200

    feat: add vizualisation

--- src/python/cocotb/conv/test_conv.py
+++ src/python/cocotb/conv/test_conv.py
@@ -52,7 +52,9 @@ def log_generics(dut):
     Log the generic parameters from the DUT in a table format.
     """
     generics = get_generics(dut)
-    table = tabulate(generics.items(), headers=["Parameter", "Value"], tablefmt="grid")
+    table = tabulate(
+        generics.items(), headers=["Parameter", "Value"], tablefmt="grid"
+    )
     dut._log.info(f"Running with generics:\n{table}")
 
 
@@ -61,7 +63,9 @@ def calculate_output_dimensions(generics):
     Calculate the output height and width based on the input dimensions and parameters.
     """
     h_out = w_out = (
-        generics["INPUT_SIZE"] + 2 * generics["PADDING"] - generics["KERNEL_SIZE"]
+        generics["INPUT_SIZE"]
+        + 2 * generics["PADDING"]
+        - generics["KERNEL_SIZE"]
     ) // generics["STRIDE"] + 1
     return h_out, w_out
 
@@ -97,9 +101,9 @@ def forward_bn(x, mean, weights, bias):
     output_torch = torch.zeros_like(x_torch)
 
     for c in range(x_torch.shape[0]):
-        output_torch[c] = (x_torch[c] - mean_torch[c]) * weights_torch[c] + bias_torch[
+        output_torch[c] = (x_torch[c] - mean_torch[c]) * weights_torch[
             c
-        ]
+        ] + bias_torch[c]
 
     return output_torch.detach().numpy()
 
@@ -112,6 +116,8 @@ def convert_output_to_int(output):
         for j in range(len(output[0][0])):
             for k in range(len(output)):
                 output[k][i][j] = output[k][i][j].signed_integer
+
+    return output
 
 
 def forward_silu(x, generics):
@@ -287,16 +293,24 @@ async def random_test(dut):
     dut.i_kernel_conv2d.value = i_kernel_conv2d
 
     i_bias_conv2d = vector_init(
-        generics["OUTPUT_CHANNELS"], bitwidth=generics["BITWIDTH"], use_random=True
+        generics["OUTPUT_CHANNELS"],
+        bitwidth=generics["BITWIDTH"],
+        use_random=True,
     )
     i_bias_bn = dut.i_weight_bn.value = vector_init(
-        generics["OUTPUT_CHANNELS"], bitwidth=generics["BITWIDTH"], use_random=True
+        generics["OUTPUT_CHANNELS"],
+        bitwidth=generics["BITWIDTH"],
+        use_random=True,
     )
     i_mean_bn = dut.i_weight_bn.value = vector_init(
-        generics["OUTPUT_CHANNELS"], bitwidth=generics["BITWIDTH"], use_random=True
+        generics["OUTPUT_CHANNELS"],
+        bitwidth=generics["BITWIDTH"],
+        use_random=True,
     )
     i_weight_bn = dut.i_weight_bn.value = vector_init(
-        generics["OUTPUT_CHANNELS"], bitwidth=generics["BITWIDTH"], use_random=True
+        generics["OUTPUT_CHANNELS"],
+        bitwidth=generics["BITWIDTH"],
+        use_random=True,
     )
 
     dut.i_bias_conv2d.value = i_bias_conv2d
@@ -419,57 +433,339 @@ def check_generics(generics):
         )
 
 
+def preprocess_layer(
+    generics: dict,
+    conv2d_layer: torch.nn.Conv2d,
+    bn_layer: torch.nn.BatchNorm2d,
+):
+    """
+    Preprocesses convolutional and batch normalization layers.
+    Scales weights, biases, and batch norm parameters for integer conversion.
+
+    Parameters:
+    ----------
+        generics (dict): Dictionary containing configuration parameters.
+        conv2d_layer (torch.nn.Conv2d): The Conv2D layer to preprocess.
+        bn_layer (torch.nn.BatchNorm2d): The BatchNorm layer to preprocess.
+
+    Returns:
+    -------
+        Tuple[List[int]]: Preprocessed convolution weights, biases, batch norm mean, weight, and bias.
+    """
+    scale_factor = 2 ** generics["DATA_SCALE_FACTOR"]
+
+    def scale_and_convert(tensor, scale_factor):
+        return (tensor * scale_factor).int().tolist()
+
+    # Convolutional layer weights and bias scaling
+    i_kernel_conv2d = scale_and_convert(conv2d_layer.weight, scale_factor)
+    i_bias_conv2d = scale_and_convert(conv2d_layer.bias, scale_factor)
+
+    epsilon = 1e-5  # A small constant to prevent division by zero
+    scaled_var = (bn_layer.running_var + epsilon).sqrt()
+    new_bn_weight = scale_and_convert(
+        bn_layer.weight / scaled_var, scale_factor
+    )
+
+    # BatchNorm mean and bias scaling
+    bn_running_mean = scale_and_convert(bn_layer.running_mean, scale_factor)
+    bn_bias = scale_and_convert(bn_layer.bias, scale_factor)
+
+    return (
+        i_kernel_conv2d,
+        i_bias_conv2d,
+        bn_running_mean,
+        new_bn_weight,
+        bn_bias,
+    )
+
+
+def preprocess_input(generics: dict, i_data: torch.Tensor):
+    """
+    Preprocesses the input data by scaling it and converting to integer format.
+
+    Parameters:
+    ----------
+        generics (dict): Configuration dictionary with scale factor.
+        i_data (torch.Tensor): Input data tensor.
+
+    Returns:
+    -------
+        List[int]: Preprocessed input data.
+    """
+    scale_factor = 2 ** generics["DATA_SCALE_FACTOR"]
+    return (i_data * scale_factor).int().tolist()
+
+
+def generate_report(generics, input, output_silu, output_hs, gotten_output):
+    import plotly.graph_objects as go
+    import torch
+    
+    # Create input inage plot
+    fig = plt.figure()
+    plt.imshow(input[0], cmap="gray")
+    plt.title("Input Image")
+    plt.axis("off")
+    plt.show()
+
+    # Flip y axis for a tensor (idk why, plotly is doing weird stuff)
+    output_hs = torch.flip(output_hs, [1])
+    output_silu = torch.flip(output_silu, [1])
+    gotten_output = torch.flip(gotten_output, [1])
+
+    # Calculate the absolute difference between the Python HS and VHDL output
+    abs_diff_hs = torch.abs(output_hs - gotten_output)
+    abs_diff_hs = abs_diff_hs.float()
+
+    avg_diff_per_channel = []
+    max_diff_per_channel = []
+    min_diff_per_channel = []
+
+    # Iterate over each channel to compute statistics
+    num_channels = abs_diff_hs.shape[0]
+    for channel in range(num_channels):
+        channel_diff = abs_diff_hs[channel]
+        avg_diff_per_channel.append(torch.mean(channel_diff).item())
+        max_diff_per_channel.append(torch.max(channel_diff).item())
+        min_diff_per_channel.append(torch.min(channel_diff).item())
+
+    # Setup the data
+    num_channels, height, width = output_hs.shape
+
+    # Create Generic Parameters for the report
+    font = dict(family="Arial, sans-serif", size=12, color="black")
+    main_title = dict(
+        text=(
+            f"<b>Convolutional Layer Report</b><br>"
+            f"<span style='font-size: 14px;'>"
+            f"Input Shape: {input.shape}, Output Shape: {output_hs.shape}<br>"
+        ),
+        x=0.0,
+        y=0.95,
+        font={"family": "Arial, sans-serif", "color": "black"},
+        xanchor="left",
+        yanchor="top",
+    )
+
+    # Only use the first channel
+    channel = 0
+
+    # Heatmap traces
+    data_hs = go.Heatmap(
+        z=output_hs[channel],
+        colorscale="Viridis",
+        visible=False,
+        coloraxis="coloraxis",
+        hovertemplate="(%{x}, %{y})<br>Value: %{z:.2f}",
+        name="",
+    )
+
+    data_silu = go.Heatmap(
+        z=output_silu[channel],
+        colorscale="Viridis",
+        visible=False,
+        coloraxis="coloraxis",
+        hovertemplate="(%{x}, %{y})<br>Value: %{z:.2f}",
+        name="",
+    )
+
+    data_gotten = go.Heatmap(
+        z=gotten_output[channel],
+        colorscale="Viridis",
+        visible=True,
+        coloraxis="coloraxis",
+        hovertemplate="(%{x}, %{y})<br>Value: %{z:.2f}",
+        name="",
+    )
+
+    data_abs_diff_hs = go.Heatmap(
+        z=abs_diff_hs[channel],
+        colorscale="Thermal",
+        visible=False,
+        coloraxis="coloraxis2",
+        hovertemplate="(%{x}, %{y})<br>Value: %{z:.2f}",
+        name="",
+    )
+
+    # Combine the data
+    data = [data_hs, data_silu, data_gotten, data_abs_diff_hs]
+
+    # Create figure
+    fig = go.Figure(data=data)
+
+    # Create buttons
+    buttons = [
+        dict(
+            label="Python HS",
+            method="update",
+            args=[
+                {"visible": [True, False, False, False]},
+                {"xaxis": {"title": "Python Result with Hardswish Function"}},
+            ],
+        ),
+        dict(
+            label="Python SiLU",
+            method="update",
+            args=[
+                {"visible": [False, True, False, False]},
+                {"xaxis": {"title": "Python Result with SiLU Function"}},
+            ],
+        ),
+        dict(
+            label="VHDL Output",
+            method="update",
+            args=[
+                {"visible": [False, False, True, False]},
+                {"xaxis": {"title": "VHDL Result"}},
+            ],
+        ),
+        dict(
+            label="Abs Diff",
+            method="update",
+            args=[
+                {"visible": [False, False, False, True]},
+                {
+                    "xaxis": {
+                        "title": "Absolute Difference between Python HS and VHDL"
+                    }
+                },
+            ],
+        ),
+    ]
+
+    # Create layout
+    fig.update_layout(
+        # Update the Menu
+        updatemenus=[
+            dict(
+                buttons=buttons,
+                direction="down",
+                pad={"r": 10, "t": 10},
+                showactive=True,
+                x=1,
+                xanchor="left",
+                y=1.1,
+                yanchor="top",
+            )
+        ],
+        coloraxis=dict(
+            colorscale="Viridis",
+            colorbar=dict(
+                title="Scale",
+                tickformat=".2e",
+                exponentformat="power",
+            ),
+        ),
+        coloraxis2=dict(
+            colorscale="Thermal",
+            colorbar=dict(
+                title="Abs Diff Scale",
+                tickvals=[
+                    min_diff_per_channel[channel],
+                    avg_diff_per_channel[channel],
+                    max_diff_per_channel[channel],
+                ],
+                tickformat=".2f",
+            ),
+        ),
+        # Update the layout
+        title=main_title,
+        xaxis_title="Python Result with Hardswish Function",
+        width=800,
+        height=800,
+        font=font,
+        margin=dict(l=20, r=20, t=40, b=20),
+    )
+
+    # Show the figure
+    fig.show()
+
+
 @cocotb.test()
 async def mnist_test(dut):
     """
-    Test the DUT's behavior with random inputs.
+    Test the DUT's behavior with random inputs from the MNIST dataset.
     """
     generics = get_generics(dut)
     check_generics(generics)
 
-    # Load Dataset
+    # Load Dataset and Model
     model, test_loader = load_dataset(PYTORCH_PATH)
 
-    # Compute output dimensions
+    # Compute output dimensions and initialize
     h_out, w_out = calculate_output_dimensions(generics)
     output_zeros = volume_init(generics["OUTPUT_CHANNELS"], h_out, w_out)
 
+    # Initialize DUT and perform reset
     await initialize_dut(dut, generics)
     await reset_dut(dut)
 
-    # Check if output is a tensor filled with zeros
+    # Assert reset state
     await assert_reset_state(dut, output_zeros)
 
-    # Get random image in dataset
+    # Enable System
+    await sys_enable_dut(dut)
+    await RisingEdge(dut.clock)
+
+    # Get random image and target from dataset
     data, target = next(iter(test_loader))
     random_index = random.randint(0, len(test_loader) - 1)
     image, label = data[random_index], target[random_index]
-    print(image.shape)
 
-    extracted_model = ExtractedNetConv(model, generics["DATA_SCALE_FACTOR"])
-    output_first_layer = extracted_model.forward_first_layer(image.unsqueeze(0))
+    # Extract the first layer's and forward it
+    extracted_model = ExtractedNetConv(
+        model, 2 ** generics["DATA_SCALE_FACTOR"]
+    )
+    output_first_layer_silu = extracted_model.forward_first_layer(
+        image.unsqueeze(0)
+    ).int()[0]
+    output_first_layer_hs = extracted_model.forward_first_layer_approximate(
+        image.unsqueeze(0)
+    ).int()[0]
 
-    # Set Input Datas
-    conv1_weight = extracted_model.conv1.weight
-    conv1_bias = extracted_model.conv1.bias
-
-    bn1_running_mean = extracted_model.bn1.running_mean
-    bn1_running_var = extracted_model.bn1.running_var
-    bn1_weight = extracted_model.bn1.weight
-    bn1_bias = extracted_model.bn1.bias
-
-    pre_computed_weight = (
-        bn1_weight / (np.sqrt(bn1_running_var)) * 2 ** generics["DATA_SCALE_FACTOR"]
+    # Preprocess Conv2D and BatchNorm layers
+    i_kernel_conv2d, i_bias_conv2d, i_mean_bn, i_weight_bn, i_bias_bn = (
+        preprocess_layer(generics, extracted_model.conv1, extracted_model.bn1)
     )
 
-    # Convert the scale factor to a power of 2
-    scale_multiplier = 2 ** generics["DATA_SCALE_FACTOR"]
+    # Preprocess input image
+    i_data_conv2d = preprocess_input(generics, image)
 
-    # TODO
-    # # Convert each value to a numpy array, scale, and then convert to integer
-    # dut.i_data_conv2d.value = np.array(image * scale_multiplier).astype(int).tolist()
-    # dut.i_kernel_conv2d.value = np.array(conv1_weight * scale_multiplier).astype(int).tolist()
-    # dut.i_bias_conv2d.value = np.array(conv1_bias * scale_multiplier).astype(int).tolist()
-    # dut.i_bias_bn.value = np.array(bn1_bias * scale_multiplier).astype(int).tolist()
-    # dut.i_mean_bn.value = np.array(bn1_running_mean * scale_multiplier).astype(int).tolist()
-    # dut.i_weight_bn.value = np.array(pre_computed_weight * scale_multiplier).astype(int).tolist()
+    # Set DUT values for Conv2D and BatchNorm
+    dut.i_data_conv2d.value = i_data_conv2d
+    dut.i_kernel_conv2d.value = i_kernel_conv2d
+    dut.i_bias_conv2d.value = i_bias_conv2d
+
+    dut.i_bias_bn.value = i_bias_bn
+    dut.i_mean_bn.value = i_mean_bn
+    dut.i_weight_bn.value = i_weight_bn
+
+    # Wait few clock cycles
+    await RisingEdge(dut.clock)
+    await RisingEdge(dut.clock)
+
+    # Set i_data_valid
+    dut.i_data_valid.value = 1
+    await RisingEdge(dut.clock)
+    dut.i_data_valid.value = 0
+    await RisingEdge(dut.clock)
+
+    # Wait until the data is valid in DUT's output
+    while dut.o_data_valid.value != 1:
+        await RisingEdge(dut.clock)
+
+    await RisingEdge(dut.clock)
+
+    # Retrieve and process DUT output
+    gotten_output = dut.o_data.value
+    gotten_output = torch.tensor(
+        np.array(convert_output_to_int(gotten_output)), dtype=torch.int32
+    )
+
+    generate_report(
+        generics,
+        image,
+        output_first_layer_silu,
+        output_first_layer_hs,
+        gotten_output,
+    )
