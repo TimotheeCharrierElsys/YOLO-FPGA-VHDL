dc789dd6f8bb0074c80dc0ae5728c5ef732380a6
timothee.charrier@elsys-design.com <timothee.charrier@elsys-design.com>
2024-09-17 17:11:22 +0200

    feat: WIP for forward features

---
+++ src/bench/mnist/test_mnist_layers.py
@@ -0,0 +1,287 @@
+import random
+import sys
+from lib import (
+    calculate_output_dimensions,
+    convert_output_to_int,
+    preprocess_layer,
+    preprocess_input,
+    postprocess_output,
+    generate_report_first_layer,
+)
+
+import cocotb
+import numpy as np
+import torch
+from cocotb.triggers import RisingEdge
+from tabulate import tabulate
+
+sys.path.insert(1, "../")
+from model import load_dataset
+from utils import (
+    assert_reset_state,
+    reset_dut,
+    setup_clock,
+    sys_enable_dut,
+    tensor_init,
+    vector_init,
+    volume_init,
+)
+
+np.set_printoptions(
+    suppress=True
+)  # prevent numpy exponential notation on print, default False
+
+# Constants
+CLOCK_PERIOD_NS = 10
+PYTORCH_PATH = r"../mnist_cnn.pt"
+
+
+def get_generics(dut):
+    """
+    Retrieve the generic parameters from the DUT.
+    """
+    return {
+        "DATA_SCALE_FACTOR": dut.DATA_SCALE_FACTOR.value,
+        "BITWIDTH": dut.BITWIDTH.value,
+        "INPUT_SIZE": dut.INPUT_SIZE.value,
+        "KERNEL_SIZE": dut.KERNEL_SIZE.value,
+        "INPUT_CHANNELS_1": dut.INPUT_CHANNELS_1.value,
+        "OUTPUT_CHANNELS_1": dut.OUTPUT_CHANNELS_1.value,
+        "PADDING": dut.PADDING.value,
+        "STRIDE_1": dut.STRIDE_1.value,
+        "OUTPUT_CHANNELS_2": dut.OUTPUT_CHANNELS_2.value,
+        "STRIDE_2": dut.STRIDE_2.value,
+    }
+
+
+def log_generics(dut):
+    """
+    Log the generic parameters from the DUT in a table format.
+    """
+    generics = get_generics(dut)
+    table = tabulate(
+        generics.items(), headers=["Parameter", "Value"], tablefmt="grid"
+    )
+    dut._log.info(f"Running with generics:\n{table}")
+
+
+async def initialize_dut(dut, generics):
+    """
+        Initialize the DUT with default values.
+                --
+
+            i_data_conv2d
+            i_kernel_conv2d
+            i_bias_conv2d_1
+    for conv2d
+
+            --
+    -- batchnorm2d
+            --
+
+    i_mean_bn_1   :
+    i_weight_bn_1 :
+    i_bias_bn_1   :
+    for batchnorm2d
+
+    conv2d input
+            i_kernel_conv2d
+            i_bias_conv2d_2
+    for conv2d
+
+    -- batchnorm2d
+    i_mean_bn_2   :
+    i_weight_bn_2 :
+    i_bias_bn_2   :
+    """
+    await setup_clock(dut)
+    dut.i_sys_enable.value = 0
+    dut.i_data_conv2d.value = volume_init(
+        generics["INPUT_CHANNELS_1"],
+        generics["INPUT_SIZE"],
+        generics["INPUT_SIZE"],
+        bitwidth=generics["BITWIDTH"],
+        use_random=False,
+    )
+    dut.i_data_valid.value = 0
+    dut.i_kernel_conv2d_1.value = tensor_init(
+        generics["OUTPUT_CHANNELS_1"],
+        generics["INPUT_CHANNELS_1"],
+        generics["KERNEL_SIZE"],
+        bitwidth=generics["BITWIDTH"],
+        use_random=False,
+    )
+    dut.i_bias_conv2d_1.value = vector_init(generics["OUTPUT_CHANNELS_1"])
+    dut.i_bias_bn_1.value = vector_init(generics["OUTPUT_CHANNELS_1"])
+    dut.i_mean_bn_1.value = vector_init(generics["OUTPUT_CHANNELS_1"])
+    dut.i_weight_bn_1.value = vector_init(generics["OUTPUT_CHANNELS_1"])
+
+    dut.i_kernel_conv2d_2.value = tensor_init(
+        generics["OUTPUT_CHANNELS_2"],
+        generics["OUTPUT_CHANNELS_1"],
+        generics["KERNEL_SIZE"],
+        bitwidth=generics["BITWIDTH"],
+        use_random=False,
+    )
+
+    dut.i_bias_conv2d_2.value = vector_init(generics["OUTPUT_CHANNELS_2"])
+    dut.i_bias_bn_2.value = vector_init(generics["OUTPUT_CHANNELS_2"])
+    dut.i_mean_bn_2.value = vector_init(generics["OUTPUT_CHANNELS_2"])
+    dut.i_weight_bn_2.value = vector_init(generics["OUTPUT_CHANNELS_2"])
+    await RisingEdge(dut.clock)
+
+
+@cocotb.test()
+async def async_reset_test(dut):
+    """
+    Test the DUT's behavior during reset.
+    """
+    generics = get_generics(dut)
+    log_generics(dut)
+
+    # Compute output dimensions
+    h_out, w_out = calculate_output_dimensions(generics)
+    output_zeros = volume_init(generics["OUTPUT_CHANNELS_2"], h_out, w_out)
+
+    await initialize_dut(dut, generics)
+    await reset_dut(dut)
+
+    # Check if output is a tensor filled with zeros
+    await assert_reset_state(dut, output_zeros)
+
+
+@cocotb.test()
+async def mnist_test_first_layer(dut):
+    """
+    Test the DUT's first Conv layer behavior with random inputs from the MNIST dataset.
+    """
+    generics = get_generics(dut)
+
+    # Load Dataset and Model
+    model, test_loader = load_dataset(PYTORCH_PATH)
+
+    # Compute output dimensions and initialize
+    h_out, w_out = calculate_output_dimensions(generics)
+    output_zeros = volume_init(generics["OUTPUT_CHANNELS_2"], h_out, w_out)
+
+    # Initialize DUT and perform reset
+    await initialize_dut(dut, generics)
+    await reset_dut(dut)
+
+    # Assert reset state
+    await assert_reset_state(dut, output_zeros)
+
+    # Enable System
+    await sys_enable_dut(dut)
+    await RisingEdge(dut.clock)
+
+    # Get random image and target from dataset
+    data, target = next(iter(test_loader))
+    random_index = random.randint(0, len(test_loader) - 1)
+    image, _ = data[random_index], target[random_index]
+
+    output_first_layer_silu = model.forward_first_layer_silu(
+        image.unsqueeze(0), 2 ** (generics["DATA_SCALE_FACTOR"])
+    ).int()[0]
+    output_first_layer_hs = model.forward_first_layer_hs(
+        image.unsqueeze(0), 2 ** (generics["DATA_SCALE_FACTOR"])
+    ).int()[0]
+
+    # Preprocess Conv2D and BatchNorm layers
+    (
+        i_kernel_conv2d_1,
+        i_bias_conv2d_1,
+        i_mean_bn_1,
+        i_weight_bn_1,
+        i_bias_bn_1,
+    ) = preprocess_layer(generics, model.conv1, model.bn1)
+
+    (
+        i_kernel_conv2d_2,
+        i_bias_conv2d_2,
+        i_mean_bn_2,
+        i_weight_bn_2,
+        i_bias_bn_2,
+    ) = preprocess_layer(generics, model.conv2, model.bn2)
+
+    # Preprocess input image
+    i_data_conv2d = preprocess_input(generics, image)
+
+    # Set DUT values for Conv2D and BatchNorm
+    dut.i_data_conv2d.value = i_data_conv2d
+    dut.i_kernel_conv2d_1.value = i_kernel_conv2d_1
+    dut.i_bias_conv2d_1.value = i_bias_conv2d_1
+
+    dut.i_bias_bn_1.value = i_bias_bn_1
+    dut.i_mean_bn_1.value = i_mean_bn_1
+    dut.i_weight_bn_1.value = i_weight_bn_1
+
+    dut.i_kernel_conv2d_2.value = i_kernel_conv2d_2
+    dut.i_bias_conv2d_2.value = i_bias_conv2d_2
+    dut.i_bias_bn_2.value = i_bias_bn_2
+    dut.i_mean_bn_2.value = i_mean_bn_2
+    dut.i_weight_bn_2.value = i_weight_bn_2
+
+    # Wait few clock cycles
+    await RisingEdge(dut.clock)
+    await RisingEdge(dut.clock)
+
+    # Set i_data_valid
+    dut.i_data_valid.value = 1
+    await RisingEdge(dut.clock)
+    dut.i_data_valid.value = 0
+    await RisingEdge(dut.clock)
+
+    # Wait until the data is valid in DUT's output
+    while dut.r_data_conv2d_1_valid.value != 1:
+        await RisingEdge(dut.clock)
+    # use get_sim_time() to get the simulation time
+    from cocotb.utils import get_sim_time
+
+    dut._log.info(f"First Layer Done in {get_sim_time('us'):.4f} us")
+    await RisingEdge(dut.clock)
+
+    # Retrieve and process DUT output
+    gotten_output = dut.r_data_conv2d_1_resized.value
+    gotten_output = torch.tensor(
+        np.array(convert_output_to_int(gotten_output)), dtype=torch.int32
+    )
+
+    # Postprocess output
+    input, output_first_layer_hs, output_first_layer_silu, gotten_output = (
+        postprocess_output(
+            image, output_first_layer_silu, output_first_layer_hs, gotten_output
+        )
+    )
+
+    # Generate report for the first layer
+    generate_report_first_layer(
+        input,
+        output_first_layer_silu,
+        output_first_layer_hs,
+        gotten_output,
+        name="First Layer",
+    )
+
+    # # Continue the test
+    output_second_layer_silu = model.forward_second_layer_silu(
+        image.unsqueeze(0), 2 ** (generics["DATA_SCALE_FACTOR"])
+    ).int()[0]
+    output_second_layer_hs = model.forward_second_layer_hs(
+        image.unsqueeze(0), 2 ** (generics["DATA_SCALE_FACTOR"])
+    ).int()[0]
+
+    # Wait until the data is valid in DUT's output
+    while dut.o_data_valid.value != 1:
+        await RisingEdge(dut.clock)
+
+    # dut._log.info(f"Second Layer Done in {get_sim_time('us'):.4f} us")
+    # await RisingEdge(dut.clock)
+
+    # # Retrieve and process DUT output
+    # gotten_output = dut.o_data.value
+    # gotten_output = torch.tensor(
+    #     np.array(convert_output_to_int(gotten_output)), dtype=torch.int32
+    # )
+
+    # output_first_layer_silu = output_first_layer_silu.float()
