f10874746ba5c44b7e89fffa1c32d8ad336ab6d7
timothee.charrier@elsys-design.com <timothee.charrier@elsys-design.com>
2024-09-27 15:43:30 +0200

    chore: configuration updates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,17 +6,15 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'HDL CNN'
-copyright = '2024, Timothée CHARRIER'
-author = 'Timothée CHARRIER'
+project = "HDL CNN"
+copyright = "2024, Timothée CHARRIER"
+author = "Timothée CHARRIER"
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
     "sphinx.ext.graphviz",
     "sphinx.ext.intersphinx",
     "sphinx_copybutton",
@@ -24,26 +22,22 @@ extensions = [
     "sphinx.ext.autosectionlabel",
 ]
 
-# Make sure the target is unique
 autosectionlabel_prefix_document = True
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
-bibtex_bibfiles = ['refs.bib']
-bibtex_encoding = 'latin'
+bibtex_bibfiles = ["refs.bib"]
+bibtex_encoding = "latin"
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'furo'
+html_theme = "furo"
 
 html_theme_options = {
     "navigation_with_keys": True,
     "top_of_page_button": "edit",
 }
 
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 pygments_style = "emacs"
 pygments_dark_style = "monokai"
